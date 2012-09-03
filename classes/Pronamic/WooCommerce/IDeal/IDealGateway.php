687fec0db12e62552dd62d04785bf66b6d3a43cd
Remco Tolsma <info@remcotolsma.nl>
2012-09-03 15:14:06 +0200

    Added support for all iDEAL variants in WooCommerce.

--- classes/Pronamic/WooCommerce/IDeal/IDealGateway.php
+++ classes/Pronamic/WooCommerce/IDeal/IDealGateway.php
@@ -22,9 +22,9 @@ class Pronamic_WooCommerce_IDeal_IDealGateway extends WC_Payment_Gateway {
 	 * Constructs and initialize an iDEAL gateway
 	 */
     public function __construct() { 
-		$this->id = self::ID;
-		$this->method_title = __('Pronamic iDEAL', 'pronamic_ideal');
-		$this->icon = plugins_url('images/icon-24x24.png', Pronamic_WordPress_IDeal_Plugin::$file);
+		$this->id           = self::ID;
+		$this->method_title = __( 'Pronamic iDEAL', 'pronamic_ideal' );
+		$this->icon         = plugins_url( 'images/icon-24x24.png', Pronamic_WordPress_IDeal_Plugin::$file );
 
 		// The iDEAL payment gateway has an issuer select field in case of the iDEAL advanced variant
 		$this->has_fields = false;
@@ -36,14 +36,14 @@ class Pronamic_WooCommerce_IDeal_IDealGateway extends WC_Payment_Gateway {
 		$this->init_settings();
 		
 		// Define user set variables
-		$this->title = $this->settings['title'];
-		$this->description = $this->settings['description'];
+		$this->title           = $this->settings['title'];
+		$this->description     = $this->settings['description'];
 		$this->configurationId = $this->settings['configuration_id'];
 		
 		// Actions
-		add_action('woocommerce_update_options_payment_gateways', array($this, 'process_admin_options'));
+		add_action( 'woocommerce_update_options_payment_gateways', array( $this, 'process_admin_options' ) );
 
-		add_action('woocommerce_receipt_' . self::ID, array($this, 'receiptPage'));
+		add_action( 'woocommerce_receipt_' . self::ID, array( $this, 'receipt_page' ) );
     } 
 
 	/**
@@ -51,36 +51,36 @@ class Pronamic_WooCommerce_IDeal_IDealGateway extends WC_Payment_Gateway {
      */
     function init_form_fields() {
     	$configurations = Pronamic_WordPress_IDeal_ConfigurationsRepository::getConfigurations();
-    	$configurationOptions = array('' => __('&mdash; Select configuration &mdash;', 'pronamic_ideal'));
-    	foreach($configurations as $configuration) {
-    		$configurationOptions[$configuration->getId()] = $configuration->getName();
+    	$configuration_options = array('' => __('&mdash; Select configuration &mdash;', 'pronamic_ideal'));
+    	foreach($configuration_options as $configuration) {
+    		$configuration_options[$configuration->getId()] = $configuration->getName();
     	}
     
     	$this->form_fields = array(
-    		'enabled' => array(
-				'title' => __('Enable/Disable', 'pronamic_ideal') , 
-				'type' => 'checkbox' , 
-				'label' => __( 'Enable iDEAL', 'pronamic_ideal') , 
+    		'enabled'          => array(
+				'title'   => __( 'Enable/Disable', 'pronamic_ideal' ) , 
+				'type'    => 'checkbox' , 
+				'label'   => __( 'Enable iDEAL', 'pronamic_ideal' ) , 
 				'default' => 'yes' 
 			) ,  
-			'title' => array(
-				'title' => __('Title', 'pronamic_ideal') , 
-				'type' => 'text' , 
-				'description' => '<br />' . __('This controls the title which the user sees during checkout.', 'pronamic_ideal') , 
-				'default' => __( 'iDEAL', 'pronamic_ideal') 
+			'title'            => array(
+				'title'       => __( 'Title', 'pronamic_ideal' ) , 
+				'type'        => 'text' , 
+				'description' => '<br />' . __( 'This controls the title which the user sees during checkout.', 'pronamic_ideal' ) , 
+				'default'     => __( 'iDEAL', 'pronamic_ideal' ) 
 			) , 
-			'description' => array(
-				'title' => __( 'Description', 'pronamic_ideal') , 
-				'type' => 'textarea' , 
-				'description' => '' . __( 'Give the customer instructions for paying via iDEAL, and let them know that their order won\'t be shipping until the money is received.', 'pronamic_ideal') , 
-				'default' => __('With iDEAL you can easily pay online in the secure environment of your own bank.', 'pronamic_ideal')
+			'description'      => array(
+				'title'       => __( 'Description', 'pronamic_ideal' ) , 
+				'type'        => 'textarea' , 
+				'description' => '' . __( 'Give the customer instructions for paying via iDEAL, and let them know that their order won\'t be shipping until the money is received.', 'pronamic_ideal' ) , 
+				'default'     => __( 'With iDEAL you can easily pay online in the secure environment of your own bank.', 'pronamic_ideal' )
 			) , 
 			'configuration_id' => array(
-				'title' => __( 'Configuration', 'pronamic_ideal') , 
-				'type' => 'select' , 
-				'description' => '<br />' . __( 'Select an iDEAL configuration.', 'pronamic_ideal') , 
-				'default' => '' , 
-				'options' => $configurationOptions 
+				'title'       => __( 'Configuration', 'pronamic_ideal' ) , 
+				'type'        => 'select' , 
+				'description' => '<br />' . __( 'Select an iDEAL configuration.', 'pronamic_ideal' ) , 
+				'default'     => '' , 
+				'options'     => $configuration_options 
 			)
 		);
     }
@@ -154,14 +154,14 @@ class Pronamic_WooCommerce_IDeal_IDealGateway extends WC_Payment_Gateway {
 	/**
 	 * Receipt page
 	 */
-	function receiptPage($order_id) {
-		$configuration = Pronamic_WordPress_IDeal_ConfigurationsRepository::getConfigurationById($this->configurationId);
+	function receipt_page( $order_id ) {
+		$configuration = Pronamic_WordPress_IDeal_ConfigurationsRepository::getConfigurationById( $this->configurationId );
 		
-		$order = new WC_Order($order_id);
+		$order = new WC_Order( $order_id );
 		
-		$dataProxy = new Pronamic_WooCommerce_IDeal_IDealDataProxy($order);
+		$dataProxy = new Pronamic_WooCommerce_IDeal_IDealDataProxy( $order );
 
-		$html = Pronamic_WordPress_IDeal_IDeal::getHtmlForm($dataProxy, $configuration);
+		$html = Pronamic_WordPress_IDeal_IDeal::getHtmlForm( $dataProxy, $configuration );
 		
 		echo $html;
 	}
@@ -192,14 +192,13 @@ class Pronamic_WooCommerce_IDeal_IDealGateway extends WC_Payment_Gateway {
 	
 			if($variant !== null) {
 				switch($variant->getMethod()) {
-					case Pronamic_IDeal_IDeal::METHOD_EASY:
-						return $this->processIDealEasyPayment($order, $configuration, $variant);
-					case Pronamic_IDeal_IDeal::METHOD_BASIC:
-						return $this->processIDealBasicPayment($order, $configuration, $variant);
-					case Pronamic_IDeal_IDeal::METHOD_OMNIKASSA:
-						return $this->processIDealOmniKassaPayment($order, $configuration, $variant);
 					case Pronamic_IDeal_IDeal::METHOD_ADVANCED:
 						return $this->processIDealAdvancedPayment($order, $configuration, $variant);
+					case Pronamic_IDeal_IDeal::METHOD_EASY:
+					case Pronamic_IDeal_IDeal::METHOD_BASIC:
+						return $this->processIDealPayment( $order, $configuration, $variant, true );
+					default: 
+						return $this->processIDealPayment( $order, $configuration, $variant, false );
 				}
 			}
 		}
@@ -250,27 +249,20 @@ class Pronamic_WooCommerce_IDeal_IDealGateway extends WC_Payment_Gateway {
 
 	//////////////////////////////////////////////////
     
-    private function processIDealEasyPayment($order, $configuration, $variant) {
-    	$this->mailCheckIDealPayment($order, $configuration);
+    /**
+     * Process iDEAL payment
+     * 
+     * @param unknown_type $order
+     * @param unknown_type $configuration
+     * @param unknown_type $variant
+     * @param unknown_type $mail_check_payment
+     * @return array
+     */
+    private function processIDealPayment( $order, $configuration, $variant, $mail_check_payment = false ) {
+    	if( $mail_check_payment ) {
+    		$this->mailCheckIDealPayment($order, $configuration);
+    	}
 
-		// Return pay page redirect
-		return array(
-			'result' 	=> 'success',
-			'redirect'	=> add_query_arg('order', $order->id, add_query_arg('key', $order->order_key, get_permalink(woocommerce_get_page_id('pay'))))
-		);
-    }
-    
-    private function processIDealBasicPayment($order, $configuration, $variant) {
-    	$this->mailCheckIDealPayment($order, $configuration);
-
-		// Return pay page redirect
-		return array(
-			'result' 	=> 'success',
-			'redirect'	=> add_query_arg('order', $order->id, add_query_arg('key', $order->order_key, get_permalink(woocommerce_get_page_id('pay'))))
-		);
-    }
-    
-    private function processIDealOmniKassaPayment($order, $configuration, $variant) {
 		// Return pay page redirect
 		return array(
 			'result' 	=> 'success',
