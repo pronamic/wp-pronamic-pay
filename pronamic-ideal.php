47d2da0119b1a7496d7c045daa28836821a5e9c9
Remco Tolsma <info@remcotolsma.nl>
2012-09-06 18:59:04 +0200

    WordPress Coding Standards optimization.

--- pronamic-ideal.php
+++ pronamic-ideal.php
@@ -16,23 +16,23 @@ Domain Path: /languages/
 License: GPL
 */
 
-if(function_exists('spl_autoload_register')):
-
-function pronamic_ideal_autoload($name) {
-	$name = str_replace('\\', DIRECTORY_SEPARATOR, $name);
-	$name = str_replace('_', DIRECTORY_SEPARATOR, $name);
-
-	$file = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'classes' . DIRECTORY_SEPARATOR . $name . '.php';
-
-	if(is_file($file)) {
-		require_once $file;
+if ( function_exists( 'spl_autoload_register' ) ) {
+	
+	function pronamic_ideal_autoload( $name ) {
+		$name = str_replace( '\\', DIRECTORY_SEPARATOR, $name );
+		$name = str_replace( '_', DIRECTORY_SEPARATOR, $name );
+	
+		$file = dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'classes' . DIRECTORY_SEPARATOR . $name . '.php';
+	
+		if ( is_file( $file ) ) {
+			require_once $file;
+		}
 	}
+	
+	spl_autoload_register( 'pronamic_ideal_autoload' );
+	
+	require_once 'functions/wp-e-commerce.php';
+	
+	Pronamic_WordPress_IDeal_Plugin::bootstrap( __FILE__ );
+
 }
-
-spl_autoload_register('pronamic_ideal_autoload');
-
-require_once 'functions/wp-e-commerce.php';
-
-Pronamic_WordPress_IDeal_Plugin::bootstrap(__FILE__);
-
-endif;
