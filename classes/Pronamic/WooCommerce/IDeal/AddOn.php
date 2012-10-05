f9c3fdb01d56b766dca81fdb9c13f28d557277d2
Remco Tolsma <info@remcotolsma.nl>
2012-10-05 12:31:44 +0200

    WordPress Coding Standards optimizations.

--- classes/Pronamic/WooCommerce/IDeal/AddOn.php
+++ classes/Pronamic/WooCommerce/IDeal/AddOn.php
@@ -22,11 +22,11 @@ class Pronamic_WooCommerce_IDeal_AddOn {
 	 * Bootstrap
 	 */
 	public static function bootstrap() {
-		add_filter('woocommerce_payment_gateways', array(__CLASS__, 'addGateway'));
+		add_filter( 'woocommerce_payment_gateways',             array( __CLASS__, 'payment_gateways' ) );
 		
-		add_action('pronamic_ideal_status_update', array(__CLASS__, 'updateStatus'), 10, 2);
+		add_action( 'pronamic_ideal_status_update',             array( __CLASS__, 'status_update' ), 10, 2 );
 		
-		add_filter('pronamic_ideal_source_column_woocommerce', array(__CLASS__, 'sourceColumn'), 10, 2);
+		add_filter( 'pronamic_ideal_source_column_woocommerce', array( __CLASS__, 'source_column' ), 10, 2 );
 	}
 
 	//////////////////////////////////////////////////
@@ -37,7 +37,7 @@ class Pronamic_WooCommerce_IDeal_AddOn {
 	 * @return true if Shopp is supported, false otherwise
 	 */
 	public static function isWooCommerceSupported() {
-		return defined('WOOCOMMERCE_VERSION');
+		return defined( 'WOOCOMMERCE_VERSION' );
 	}
 
 	//////////////////////////////////////////////////
@@ -45,10 +45,10 @@ class Pronamic_WooCommerce_IDeal_AddOn {
 	/**
 	 * Add the gateway to WooCommerce
 	 */
-	function addGateway($methods) {
-		$methods[] = 'Pronamic_WooCommerce_IDeal_IDealGateway';
+	function payment_gateways( $gateways ) {
+		$gateways[] = 'Pronamic_WooCommerce_IDeal_IDealGateway';
 
-		return $methods;
+		return $gateways;
 	}
 
 	//////////////////////////////////////////////////
@@ -58,56 +58,56 @@ class Pronamic_WooCommerce_IDeal_AddOn {
 	 * 
 	 * @param string $payment
 	 */
-	public static function updateStatus(Pronamic_WordPress_IDeal_Payment $payment, $canRedirect = false) {
-		if($payment->getSource() == self::SLUG && self::isWooCommerceSupported()) {
+	public static function status_update( Pronamic_WordPress_IDeal_Payment $payment, $can_redirect = false ) {
+		if ( $payment->getSource() == self::SLUG && self::isWooCommerceSupported() ) {
 			$id = $payment->getSourceId();
 			$transaction = $payment->transaction;
 
-			$order = new WC_Order((int) $id);
-			$dataProxy = new Pronamic_WooCommerce_IDeal_IDealDataProxy($order);
+			$order = new WC_Order( (int) $id );
+			$data_proxy = new Pronamic_WooCommerce_IDeal_IDealDataProxy( $order );
 
-			if($order->status !== Pronamic_WooCommerce_WooCommerce::ORDER_STATUS_COMPLETED) {				
-				$url = $dataProxy->getNormalReturnUrl();
+			if ( $order->status !== Pronamic_WooCommerce_WooCommerce::ORDER_STATUS_COMPLETED ) {				
+				$url = $data_proxy->getNormalReturnUrl();
 
 				$status = $transaction->getStatus();
 
-				switch($status) {
+				switch ( $status ) {
 					case Pronamic_IDeal_Transaction::STATUS_CANCELLED:
-						$order->update_status(Pronamic_WooCommerce_WooCommerce::ORDER_STATUS_CANCELLED, __('iDEAL payment cancelled.', 'pronamic_ideal'));
+						$order->update_status( Pronamic_WooCommerce_WooCommerce::ORDER_STATUS_CANCELLED, __( 'iDEAL payment cancelled.', 'pronamic_ideal' ) );
 
-						$url = $dataProxy->getCancelUrl();
+						$url = $data_proxy->getCancelUrl();
 
 						break;
 					case Pronamic_IDeal_Transaction::STATUS_EXPIRED:
 						// WooCommerce PayPal gateway uses 'failed' order status for an 'expired' payment
 						// @see http://plugins.trac.wordpress.org/browser/woocommerce/tags/1.5.4/classes/gateways/class-wc-paypal.php#L557
-						$order->update_status(Pronamic_WooCommerce_WooCommerce::ORDER_STATUS_FAILED, __('iDEAL payment expired.', 'pronamic_ideal'));
+						$order->update_status( Pronamic_WooCommerce_WooCommerce::ORDER_STATUS_FAILED, __( 'iDEAL payment expired.', 'pronamic_ideal' ) );
 
 						break;
 					case Pronamic_IDeal_Transaction::STATUS_FAILURE:
-						$order->update_status(Pronamic_WooCommerce_WooCommerce::ORDER_STATUS_FAILED, __('iDEAL payment failed.', 'pronamic_ideal'));
+						$order->update_status( Pronamic_WooCommerce_WooCommerce::ORDER_STATUS_FAILED, __( 'iDEAL payment failed.', 'pronamic_ideal' ) );
 
 						break;
 					case Pronamic_IDeal_Transaction::STATUS_SUCCESS:
 		            	// Payment completed
-		                $order->add_order_note(__('iDEAL payment completed.', 'pronamic_ideal'));
+		                $order->add_order_note( __( 'iDEAL payment completed.', 'pronamic_ideal' ) );
 		                $order->payment_complete();
 		                
-		                $url = $dataProxy->getSuccessUrl();
+		                $url = $data_proxy->getSuccessUrl();
 
 						break;
 					case Pronamic_IDeal_Transaction::STATUS_OPEN:
-						$order->add_order_note(__('iDEAL payment open.', 'pronamic_ideal'));
+						$order->add_order_note( __( 'iDEAL payment open.', 'pronamic_ideal' ) );
 
 						break;
 					default:
-						$order->add_order_note(__('iDEAL payment unknown.', 'pronamic_ideal'));
+						$order->add_order_note( __( 'iDEAL payment unknown.', 'pronamic_ideal' ) );
 
 						break;
 				}
 				
-				if($canRedirect) {
-					wp_redirect($url, 303);
+				if ( $can_redirect ) {
+					wp_redirect( $url, 303 );
 
 					exit;
 				}
@@ -120,12 +120,16 @@ class Pronamic_WooCommerce_IDeal_AddOn {
 	/**
 	 * Source column
 	 */
-	public static function sourceColumn($text, $payment) {
+	public static function source_column( $text, $payment ) {
 		$text  = '';
-		$text .= __('WooCommerce', 'pronamic_ideal') . '<br />';
-		$text .= sprintf('<a href="%s">', get_edit_post_link($payment->getSourceId()));
-		$text .= sprintf(__('Order #%s', 'pronamic_ideal'), $payment->getSourceId());
-		$text .= '</a>';
+
+		$text .= __( 'WooCommerce', 'pronamic_ideal' ) . '<br />';
+
+		$text .= sprintf(
+			'<a href="%s">%s</a>', 
+			get_edit_post_link( $payment->getSourceId() ),
+			sprintf( __( 'Order #%s', 'pronamic_ideal' ), $payment->getSourceId() )
+		);
 
 		return $text;
 	}
