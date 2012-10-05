09d0faa4b0674d3ff9b75faca2b95622c7c4e5fa
Remco Tolsma <info@remcotolsma.nl>
2012-10-05 10:59:45 +0200

    WordPress Coding Standards optimizations.

--- views/variants.php
+++ views/variants.php
@@ -2,36 +2,36 @@
 	<?php screen_icon( 'pronamic_ideal' ); ?>
 
 	<h2>
-		<?php _e('iDEAL Variants', 'pronamic_ideal'); ?>
+		<?php _e( 'iDEAL Variants', 'pronamic_ideal' ); ?>
 	</h2>
 
 	<table cellspacing="0" class="widefat fixed">
 
-		<?php foreach(array('thead', 'tfoot') as $tag): ?>
+		<?php foreach ( array( 'thead', 'tfoot' ) as $tag ): ?>
 
-		<<?php echo $tag; ?>>
-			<tr>
-				<th scope="col" class="manage-column"><?php _e( 'Provider', 'pronamic_ideal' ); ?></th>
-				<th scope="col" class="manage-column"><?php _e( 'Name', 'pronamic_ideal' ); ?></th>
-				<th scope="col" class="manage-column"><?php _e( 'Type', 'pronamic_ideal' );  ?></th>
-				<th scope="col" class="manage-column"><?php _e( 'Method', 'pronamic_ideal' );  ?></th>
-				<th scope="col" class="manage-column"><?php _e( 'Feedback Payment Status', 'pronamic_ideal' );  ?></th>
-				<th scope="col" class="manage-column"><?php _e( 'Dashboard', 'pronamic_ideal' ); ?></th>
-			</tr>
-		</<?php echo $tag; ?>>
+			<<?php echo $tag; ?>>
+				<tr>
+					<th scope="col" class="manage-column"><?php _e( 'Provider', 'pronamic_ideal' ); ?></th>
+					<th scope="col" class="manage-column"><?php _e( 'Name', 'pronamic_ideal' ); ?></th>
+					<th scope="col" class="manage-column"><?php _e( 'Type', 'pronamic_ideal' );  ?></th>
+					<th scope="col" class="manage-column"><?php _e( 'Method', 'pronamic_ideal' );  ?></th>
+					<th scope="col" class="manage-column"><?php _e( 'Feedback Payment Status', 'pronamic_ideal' );  ?></th>
+					<th scope="col" class="manage-column"><?php _e( 'Dashboard', 'pronamic_ideal' ); ?></th>
+				</tr>
+			</<?php echo $tag; ?>>
 
 		<?php endforeach; ?>
 
 		<tbody>
 
-			<?php foreach(Pronamic_WordPress_IDeal_ConfigurationsRepository::getVariants() as $variant): ?>
+			<?php foreach ( Pronamic_WordPress_IDeal_ConfigurationsRepository::getVariants() as $variant ): ?>
 
 			<tr>
 				<td>
-					<?php if($provider = $variant->getProvider()): ?>
-					<a href="<?php echo $provider->getUrl(); ?>">
-						<?php echo $provider->getName(); ?>
-					</a>
+					<?php if ( $provider = $variant->getProvider() ): ?>
+						<a href="<?php echo $provider->getUrl(); ?>">
+							<?php echo $provider->getName(); ?>
+						</a>
 					<?php endif; ?>
 				</td>
 				<td>
@@ -40,21 +40,21 @@
 				<td>
 					<?php 
 					
-					switch($variant->getMethod()) {
+					switch ( $variant->getMethod() ) {
 						case Pronamic_IDeal_IDeal::METHOD_EASY:
-							_e('Easy', 'pronamic_ideal');
+							_e( 'Easy', 'pronamic_ideal' );
 							break;
 						case Pronamic_IDeal_IDeal::METHOD_BASIC:
-							_e('Basic', 'pronamic_ideal');
+							_e( 'Basic', 'pronamic_ideal' );
 							break;
 						case Pronamic_IDeal_IDeal::METHOD_OMNIKASSA:
-							_e('OmniKassa', 'pronamic_ideal');
+							_e( 'OmniKassa', 'pronamic_ideal' );
 							break;
 						case Pronamic_IDeal_IDeal::METHOD_ADVANCED:
-							_e('Advanced', 'pronamic_ideal');
+							_e( 'Advanced', 'pronamic_ideal' );
 							break;
 						default:
-							_e('Unknown', 'pronamic_ideal');
+							_e( 'Unknown', 'pronamic_ideal' );
 							break;
 					}
 					
@@ -83,16 +83,16 @@
 					<?php $variant->feedback_payment_status ? _e( 'Yes', 'pronamic_ideal' ) : _e( 'No', 'pronamic_ideal ' ); ?>
 				</td>
 				<td>
-					<?php if($variant->testSettings->dashboardUrl): ?>
-					<a href="<?php echo $variant->testSettings->dashboardUrl; ?>">
-						<?php _e('Test', 'pronamic_ideal'); ?>
-					</a>
+					<?php if ( $variant->testSettings->dashboardUrl ): ?>
+						<a href="<?php echo $variant->testSettings->dashboardUrl; ?>">
+							<?php _e( 'Test', 'pronamic_ideal' ); ?>
+						</a>
 					<?php endif; ?>
 
-					<?php if($variant->liveSettings->dashboardUrl): ?>
-					<a href="<?php echo $variant->liveSettings->dashboardUrl; ?>">
-						<?php _e('Live', 'pronamic_ideal'); ?>
-					</a>
+					<?php if ( $variant->liveSettings->dashboardUrl ): ?>
+						<a href="<?php echo $variant->liveSettings->dashboardUrl; ?>">
+							<?php _e( 'Live', 'pronamic_ideal' ); ?>
+						</a>
 					<?php endif; ?>
 				</td>
 			</tr>
