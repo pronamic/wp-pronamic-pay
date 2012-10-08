39edb93cfc3ec01a974505d38b22d12d777d0b45
Remco Tolsma <info@remcotolsma.nl>
2012-10-08 11:35:47 +0200

    Added http:// to Facebok URL.

--- views/pronamic.php
+++ views/pronamic.php
@@ -3,7 +3,7 @@
 <h3>Facebook</h3>
 
 <div style="margin: 1em 0;">
-	<iframe src="//www.facebook.com/plugins/like.php?href=http%3A%2F%2Fwww.facebook.com%2FPronamic&amp;send=false&amp;layout=standard&amp;width=600&amp;show_faces=false&amp;action=like&amp;colorscheme=light&amp;font&amp;height=35&amp;appId=208724019165734" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:600px; height:35px;" allowTransparency="true"></iframe>
+	<iframe src="http://www.facebook.com/plugins/like.php?href=http%3A%2F%2Fwww.facebook.com%2FPronamic&amp;send=false&amp;layout=standard&amp;width=600&amp;show_faces=false&amp;action=like&amp;colorscheme=light&amp;font&amp;height=35&amp;appId=208724019165734" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:600px; height:35px;" allowTransparency="true"></iframe>
 </div>
 
 <h3>Twitter</h3>
