9dd1efa893ba1b69d91e1cebd6d378a5a82cb94f
Remco Tolsma <info@remcotolsma.nl>
2012-12-13 16:19:37 +0100

    Removed unused helper functions.

--- classes/Pronamic/IDeal/HTML/Helper.php
+++ classes/Pronamic/IDeal/HTML/Helper.php
@@ -29,60 +29,6 @@ class Pronamic_IDeal_HTML_Helper {
 		return $html;
 	}
 	
-	/**
-	 * Create an issuer HTML select element
-	 * 
-	 * @param string $name
-	 * @param array $lists
-	 * @param string $emptyOption
-	 * @param array $groups
-	 */
-	public static function issuersSelect( $name, array $lists, $empty_option = null, array $groups = array(), $id = '' ) {
-		$attributes = array(
-			'id'   => $id,
-			'name' => $name, 
-		);
-		
-		$html = sprintf(
-			'<select %s>%s</select>',
-			self::array_to_html_attributes( $attributes ),
-			self::issuersSelectOptions( $lists, $empty_option, $groups )
-		);
-		
-		return $html;
-	}
-
-	/**
-	 * Create isseur HTML select options
-	 * 
-	 * @param array $lists
-	 * @param string $emptyOption
-	 * @param array $groups
-	 */
-	public static function issuersSelectOptions( array $lists, $empty_option = null, $value, array $groups = array() ) {
-		$html  = '';
-
-		if ( $empty_option !== null ) {
-			$html .= '<option value="">' . $empty_option . '</option>';
-		}
-		
-		foreach ( $lists as $name => $list ) {
-			if ( isset( $groups[$name] ) ) {
-				$html .= '<optgroup label="' . $groups[$name] . '">';
-			}
-
-			foreach ( $list as $issuer ) {
-				$html .= '<option value="' . $issuer->getId() . '" ' . selected( $value, $issuer->getId(), false ) . '>' . $issuer->getName() . '</option>';
-			}
-
-			if ( isset( $groups[$name] ) ) {
-				$html .= '</optgroup>';
-			}
-		}
-		
-		return $html;
-	}
-	
 	public static function select_options_grouped( $groups, $selected_value = null ) {
 		$html = '';
 
