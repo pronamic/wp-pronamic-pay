79aafd3c22151f31c7ad37a0553945daad018e6d
Remco Tolsma <info@remcotolsma.nl>
2012-12-13 12:06:01 +0100

    Improved the old iDEAL Advanced gateway.

--- classes/Pronamic/Gateways/IDealAdvanced/Gateway.php
+++ classes/Pronamic/Gateways/IDealAdvanced/Gateway.php
@@ -17,13 +17,87 @@ class Pronamic_Gateways_IDealAdvanced_Gateway extends Pronamic_Gateways_Gateway 
 		$this->set_require_issue_select( true );
 		$this->set_amount_minimum( 0.01 );
 
+		// Client
+		$client = new Pronamic_Gateways_IDealAdvanced_Client();
+		$client->setAcquirerUrl( $configuration->getPaymentServerUrl() );
+		$client->setAcquirerUrl($configuration->getPaymentServerUrl());
+		$client->setPrivateKey($configuration->privateKey);
+		$client->setPrivateKeyPassword($configuration->privateKeyPassword);
+		$client->setPrivateCertificate($configuration->privateCertificate);
+
+		$variant = $configuration->getVariant();
+		foreach($variant->certificates as $certificate) {
+			$client->addPublicCertificate($certificate);
+		}
 		
-		
+		$this->client = $client;
+		$this->configuration = $configuration;	
 	}
 	
 	/////////////////////////////////////////////////
 
 	public function get_issuers() {
+		$client = $this->client;
+		$configuration = $this->configuration;
+			
+		$message = new Pronamic_Gateways_IDealAdvanced_XML_DirectoryRequestMessage();
+		$merchant = $message->getMerchant();
+		$merchant->id = $configuration->getMerchantId();
+		$merchant->subId = $configuration->getSubId();
+		$merchant->authentication = Pronamic_IDeal_IDeal::AUTHENTICATION_SHA1_RSA;
+		$merchant->token = Pronamic_Gateways_IDealAdvanced_Security::getShaFingerprint($configuration->privateCertificate);
+		$message->sign($configuration->privateKey, $configuration->privateKeyPassword);
+
+		$result = $client->getIssuerLists($message);
+
+		$lists = null;
 		
+		if($result !== null) {
+			$lists = $result;
+		} elseif($error = $client->getError()) {
+			$this->error = $error;
+		}
+		
+		return $lists;
+	}
+	
+	/////////////////////////////////////////////////
+
+	public function get_issuer_field() {
+		$choices = array();
+		
+		$list = $this->get_issuers();
+		
+		foreach( $list as $name => $issuers ) {
+			$options = array();
+
+			foreach ( $issuers as $issuer ) {
+				$options[$issuer->getId()] = $issuer->getName(); 
+			}
+	
+			$choices[] = array(
+				'name'    => ( $name == 'Long' ) ? __( '&mdash; Other banks &mdash;', 'pronamic_ideal' ) : false,
+				'options' => $options
+			);
+		}
+		
+		return array(
+			'id'       => 'pronamic_ideal_issuer_id',
+			'name'     => 'pronamic_ideal_issuer_id',
+			'label'    => __( 'Choose your bank', 'pronamic_ideal' ),
+			'required' => true,
+			'type'     => 'select',
+			'choices'  => $choices
+		);
+	}
+	
+	/////////////////////////////////////////////////
+
+	public function get_input_fields() {
+		$fields = array();
+		
+		$fields[] = $this->get_issuer_field(); 
+
+		return $fields;
 	}
 }
