3d8a1ec173ffc9cd58ecb6632342fd3deb41f8eb
Remco Tolsma <info@remcotolsma.nl>
2012-12-17 13:38:23 +0100

    Removed unused show_message function.

--- classes/Pronamic/Shopp/IDeal/AddOn.php
+++ classes/Pronamic/Shopp/IDeal/AddOn.php
@@ -36,10 +36,6 @@ class Pronamic_Shopp_IDeal_AddOn {
 		
 		// Filters
 		add_filter( 'pronamic_ideal_source_column_shopp', array( __CLASS__, 'source_column' ), 10, 2 );
-
-		add_filter( 'shopp_checkout_form',                array( __CLASS__, 'show_message' ) );
-		add_filter( 'shopp_cart_template',                array( __CLASS__, 'show_message' ) );
-		add_filter( 'shopp_catalog',                      array( __CLASS__, 'show_message' ) );
 	}
 
 	//////////////////////////////////////////////////
@@ -49,7 +45,7 @@ class Pronamic_Shopp_IDeal_AddOn {
 	 * 
 	 * @return true if Shopp is supported, false otherwise
 	 */
-	public static function isShoppSupported() {
+	public static function is_shopp_supported() {
 		return defined( 'SHOPP_VERSION' );
 	}
 
@@ -88,7 +84,7 @@ class Pronamic_Shopp_IDeal_AddOn {
 	 * @param Pronamic_WordPress_IDeal_Payment $payment
 	 */
 	public static function update_status( Pronamic_WordPress_IDeal_Payment $payment, $can_redirect = false ) {
-		if ( $payment->getSource() == self::SLUG && self::isShoppSupported() ) {
+		if ( $payment->getSource() == self::SLUG && self::is_shopp_supported() ) {
 			global $Shopp;
 
 			$id = $payment->getSourceId();
@@ -138,49 +134,6 @@ class Pronamic_Shopp_IDeal_AddOn {
 				}
 			}
 		}
-	}
-	
-	//////////////////////////////////////////////////
-	
-	/**
-	 * Gets the $_GET['mesagetype'] from the url and returns
-	 * the queued page with the message to Shopp.
-	 */
-	public static function show_message( $output ) {
-		$message = '';
-
-		// Pick something to display
-		switch ( strtolower( $_GET['messagetype'] ) ) {
-			case 'cancelled':
-				$message .= __( 'Payment of the order has been cancelled.', 'pronamic_ideal' );
-				break;
-			case 'error':
-				$message .= __( 'An unexpected error occured during transaction.', 'pronamic_ideal' );
-				break;
-			case 'expired':
-				$message .= __( 'The maximum transaction time expired.', 'pronamic_ideal' );
-				break;
-			case 'failure':
-				$message .= __( 'The transaction failed for an unknown reason.', 'pronamic_ideal' );
-				break;
-			case 'open':
-				$message .= __( 'Transaction was not finished and was left open.', 'pronamic_ideal' );
-				break;
-		}
-		
-		// Display an error message when message has a value
-		$messageoutput = '';
-		$error = new ShoppError( $message );
-		if ( ! empty( $message ) ) {
-			$messageoutput .= '<div id="errors" class="shopp">
-				<h3>' . __( 'Errors', 'pronamic_ideal' ) . '</h3>
-				<p>
-					' . $error->message() . '
-				</p>
-			</div>';
-		}
-		
-		return $messageoutput . $output;
 	}
 	
 	//////////////////////////////////////////////////
