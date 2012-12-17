d9a02d0d0f8c458d87406d7a3fae237b6900e419
Remco Tolsma <info@remcotolsma.nl>
2012-12-17 15:47:00 +0100

    WordPress Coding Standards optimizations.

--- includes/wp-e-commerce.php
+++ includes/wp-e-commerce.php
@@ -1,9 +1,9 @@
 <?php
 
 function pronamic_ideal_wpsc_merchant_form() {
-	return Pronamic_WPeCommerce_IDeal_IDealMerchant::adminConfigurationForm();
+	return Pronamic_WPeCommerce_IDeal_IDealMerchant::admin_configuration_form();
 }
 
 function pronamic_ideal_wpsc_merchant_submit_function() {
-	return Pronamic_WPeCommerce_IDeal_IDealMerchant::adminConfigurationSubmit();
+	return Pronamic_WPeCommerce_IDeal_IDealMerchant::admin_configuration_submit();
 }
