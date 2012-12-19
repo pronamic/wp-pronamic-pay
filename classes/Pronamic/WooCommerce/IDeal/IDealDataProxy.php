8f4182bb0a8c76b27bcec1982c3697b9f044c85b
Remco Tolsma <info@remcotolsma.nl>
2012-12-19 11:52:49 +0100

    Improved documentation.

--- classes/Pronamic/WooCommerce/IDeal/IDealDataProxy.php
+++ classes/Pronamic/WooCommerce/IDeal/IDealDataProxy.php
@@ -75,13 +75,21 @@ class Pronamic_WooCommerce_IDeal_IDealDataProxy extends Pronamic_WordPress_IDeal
 		 * detail           = System generating error: issuer
 		 * consumer_message = Paying with iDEAL is not possible. Please try again later or pay another way.
 		 * 
+		 * Or in case of Sisow:
+		 * <errorresponse xmlns="https://www.sisow.nl/Sisow/REST" version="1.0.0">
+		 *     <error>
+		 *         <errorcode>TA3230</errorcode>
+		 *         <errormessage>No purchaseid</errormessage>
+		 *     </error>
+		 * </errorresponse>
+		 * 
 		 * @see http://wcdocs.woothemes.com/user-guide/extensions/functionality/sequential-order-numbers/#add-compatibility
 		 * 
 		 * @see page 30 http://pronamic.nl/wp-content/uploads/2012/09/iDEAL-Merchant-Integratie-Gids-NL.pdf
 		 * 
-		 * Gebruik van een hierboven niet genoemd teken leidt niet tot weigering van batch of post, maar het
-		 * teken wordt door Equens (voorheen Interpay) naar spatie, vraagteken of asterisk vertaald. Dit geldt dus
-		 * ook voor diakritische tekens (à, ç, ô, ü, ý enzovoorts).
+		 * The use of characters that are not listed above will not lead to a refusal of a batch or post, but the 
+		 * character will be changed by Equens (formerly Interpay) to a space, question mark or asterisk. The 
+		 * same goes for diacritical characters (à, ç, ô, ü, ý etcetera).
 		 */
 		$order_id = str_replace( '#', '', $order_id );
 
