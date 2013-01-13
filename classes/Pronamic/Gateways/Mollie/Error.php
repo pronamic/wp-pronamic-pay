dfe4abde6a5368579da1c9db3adf52feec0a8ddb
Remco Tolsma <info@remcotolsma.nl>
2013-01-13 13:29:48 +0100

    Improved Mollie error documentation.

--- classes/Pronamic/Gateways/Mollie/Error.php
+++ classes/Pronamic/Gateways/Mollie/Error.php
@@ -10,11 +10,45 @@
  * @see https://www.mollie.nl/support/documentatie/betaaldiensten/ideal/en/
  */
 class Pronamic_Gateways_Mollie_Error {
+	/**
+	 * Mollie error code
+	 * 
+	 * @var string
+	 */
 	private $code;
 
+	/**
+	 * Mollie error message
+	 * 
+	 * @var string
+	 */
 	private $message;
+	
+	/////////////////////////////////////////////////
 
+	/**
+	 * Constructs and initializes an Mollie error object
+	 * 
+	 * @param string $code
+	 * @param string $message
+	 */
 	public function __construct( $code, $message ) {
-		
+		$this->code    = $code;
+		$this->message = $message;
+	}
+	
+	//////////////////////////////////////////////////
+
+	// @todo getters and setters
+	
+	//////////////////////////////////////////////////
+
+	/**
+	 * Create an string representation of this object
+	 * 
+	 * @return string
+	 */
+	public function __toString() {
+		return $this->code . ' ' . $this->message;
 	}
 }
