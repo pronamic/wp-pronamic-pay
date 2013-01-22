1a4c1bcda0d2649f7a2b7621d803fedcc2a0f372
Remco Tolsma <info@remcotolsma.nl>
2013-01-22 12:24:59 +0100

    WordPress Coding Standards optimizations.

--- views/configurations.php
+++ views/configurations.php
@@ -2,13 +2,13 @@
 	<?php screen_icon( 'pronamic_ideal' ); ?>
 
 	<h2>
-		<?php _e('iDEAL Configurations', 'pronamic_ideal');
+		<?php _e( 'iDEAL Configurations', 'pronamic_ideal' );
 
-		if(true): ?>
-
-		<a class="button add-new-h2" href="<?php echo Pronamic_WordPress_IDeal_Admin::getConfigurationEditLink(); ?>">
-			<?php _e('Add New', 'pronamic_ideal'); ?>
-		</a>
+		if ( true ) : ?>
+	
+			<a class="button add-new-h2" href="<?php echo Pronamic_WordPress_IDeal_Admin::getConfigurationEditLink(); ?>">
+				<?php _e( 'Add New', 'pronamic_ideal' ); ?>
+			</a>
 
 		<?php endif; ?>
 	</h2>
