bf2698ece6e525751160fee396f39ca1d60c044b
Remco Tolsma <info@remcotolsma.nl>
2013-02-02 11:48:33 +0100

    Added an payment statuses class for some constants.

---
+++ classes/Pronamic/ClassiPress/PaymentStatuses.php
@@ -0,0 +1,27 @@
+<?php 
+
+/**
+ * Title: Payment statuses
+ * Description: 
+ * Copyright: Copyright (c) 2005 - 2011
+ * Company: Pronamic
+ * @author Remco Tolsma
+ * @version 1.0
+ */
+class Pronamic_ClassiPress_PaymentStatuses {
+	/**
+	 * Indiactor for the 'Completed' payment status
+	 * 
+	 * @var string
+	 * @see https://bitbucket.org/Pronamic/classipress/src/bc1334736c6e/includes/admin/admin-options.php?at=3.2.1#cl-2717
+	 */
+	const COMPLETED = 'Completed';
+
+	/**
+	 * Indiactor for the 'Pending' payment status
+	 * 
+	 * @var string
+	 * @see https://bitbucket.org/Pronamic/classipress/src/bc1334736c6e/includes/admin/admin-options.php?at=3.2.1#cl-2730
+	 */
+	const PENDING = 'Pending';
+}
