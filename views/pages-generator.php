cff767c8a4ca97f690c5aff54f6f8dc0ccf560f2
Remco Tolsma <info@remcotolsma.nl>
2013-02-08 13:56:58 +0100

    Auto SEO no index meta key for generated pages.

--- views/pages-generator.php
+++ views/pages-generator.php
@@ -1,32 +1,38 @@
 <?php 
 
-function pronamic_ideal_create_pages($pages, $parent = null) {
-	foreach($pages as $page) {
+function pronamic_ideal_create_pages( $pages, $parent = null ) {
+	foreach ( $pages as $page ) {
 		$post = array(
-			'post_title' => $page['post_title'] ,
-			'post_name' => $page['post_title'] ,
-			'post_content' => $page['post_content'] ,  
-			'post_status' => 'publish' , 
-			'post_type' => 'page' , 
+			'post_title'     => $page['post_title'] ,
+			'post_name'      => $page['post_title'] ,
+			'post_content'   => $page['post_content'] ,  
+			'post_status'    => 'publish' , 
+			'post_type'      => 'page' , 
 			'comment_status' => 'closed' 
 		);
 		
-		if(isset($parent)) {
+		if ( isset( $parent ) ) {
 			$post['post_parent'] = $parent;
 		}
 
-		$result = wp_insert_post($post, true);
+		$result = wp_insert_post( $post, true );
 		
-		if(!is_wp_error($result)) {
-			if(isset($page['children'])) {
-				pronamic_ideal_create_pages($page['children'], $result);
+		if ( ! is_wp_error( $result ) ) {
+			if ( isset( $page['post_meta'] ) ) {
+				foreach ( $page['post_meta'] as $key => $value ) {
+					update_post_meta( $result, $key, $value );
+				}
+			}
+			
+			if ( isset( $page['children'] ) ) {
+				pronamic_ideal_create_pages( $page['children'], $result );
 			}
 		}
 	}
 }
 
-if(!empty($_POST) && check_admin_referer('pronamic_ideal_pages_generator', 'pronamic_ideal_nonce')) {
-	pronamic_ideal_create_pages($_POST['pronamic_ideal_pages']);
+if ( ! empty( $_POST ) && check_admin_referer( 'pronamic_ideal_pages_generator', 'pronamic_ideal_nonce' ) ) {
+	pronamic_ideal_create_pages( $_POST['pronamic_ideal_pages'] );
 }
 
 ?>
@@ -42,97 +48,138 @@ if(!empty($_POST) && check_admin_referer('pronamic_ideal_pages_generator', 'pron
 	</p>
 
 	<form action="" method="post">
-		<?php wp_nonce_field('pronamic_ideal_pages_generator', 'pronamic_ideal_nonce'); ?>
+		<?php wp_nonce_field( 'pronamic_ideal_pages_generator', 'pronamic_ideal_nonce' ); ?>
 
 		<?php 
 		
 		$pages = array(
 			'ideal' => array(
-				'post_title' => __('iDEAL', 'pronamic_ideal') , 
-				'post_name' => __('ideal', 'pronamic_ideal') ,
-				'post_content' => '' ,  
-				'children' => array(
-					'error' => array(
-						'post_title' => __('iDEAL payment error', 'pronamic_ideal') , 
-						'post_name' => __('error', 'pronamic_ideal') ,
-						'post_content' => __('<p>Unfortunately an error has occurred during your iDEAL payment.</p>', 'pronamic_ideal')
+				'post_title'   => __( 'iDEAL', 'pronamic_ideal' ), 
+				'post_name'    => __( 'ideal', 'pronamic_ideal' ),
+				'post_content' => '',
+				'post_meta'    => array(
+					'_yoast_wpseo_meta-robots-noindex' =>true
+				),
+				'children'     => array(
+					'error'     => array(
+						'post_title'   => __( 'iDEAL payment error', 'pronamic_ideal' ), 
+						'post_name'    => __( 'error', 'pronamic_ideal' ),
+						'post_content' => __( '<p>Unfortunately an error has occurred during your iDEAL payment.</p>', 'pronamic_ideal' ),
+						'post_meta'    => array(
+							'_yoast_wpseo_meta-robots-noindex' =>true
+						)
 					) , 
-					'cancel' => array(
-						'post_title' => __('iDEAL payment canceled', 'pronamic_ideal') , 
-						'post_name' => __('cancelled', 'pronamic_ideal') ,
-						'post_content' => __('<p>You canceled the iDEAL payment.</p>', 'pronamic_ideal')
+					'cancel'    => array(
+						'post_title'   => __( 'iDEAL payment canceled', 'pronamic_ideal' ),
+						'post_name'    => __( 'cancelled', 'pronamic_ideal' ),
+						'post_content' => __( '<p>You canceled the iDEAL payment.</p>', 'pronamic_ideal' ),
+						'post_meta'    => array(
+							'_yoast_wpseo_meta-robots-noindex' =>true
+						)
 					) , 
-					'unknown' => array(
-						'post_title' => __('iDEAL payment unknown', 'pronamic_ideal') , 
-						'post_name' => __('unknown', 'pronamic_ideal') ,
-						'post_content' => __('<p>The status of your iDEAL payment is unknown.</p>', 'pronamic_ideal')
+					'unknown'   => array(
+						'post_title'   => __( 'iDEAL payment unknown', 'pronamic_ideal'),
+						'post_name'    => __( 'unknown', 'pronamic_ideal' ),
+						'post_content' => __( '<p>The status of your iDEAL payment is unknown.</p>', 'pronamic_ideal' ),
+						'post_meta'    => array(
+							'_yoast_wpseo_meta-robots-noindex' =>true
+						)
 					) , 
-					'expired' => array(
-						'post_title' => __('iDEAL payment expired', 'pronamic_ideal') , 
-						'post_name' => __('expired', 'pronamic_ideal') ,
-						'post_content' => __('<p>Unfortunately your iDEAL payment session has expired.</p>', 'pronamic_ideal')
-					) , 
+					'expired'   => array(
+						'post_title'   => __( 'iDEAL payment expired', 'pronamic_ideal' ),
+						'post_name'    => __( 'expired', 'pronamic_ideal' ),
+						'post_content' => __( '<p>Unfortunately your iDEAL payment session has expired.</p>', 'pronamic_ideal' ),
+						'post_meta'    => array(
+							'_yoast_wpseo_meta-robots-noindex' =>true
+						)
+					), 
 					'completed' => array(
-						'post_title' => __('iDEAL payment completed', 'pronamic_ideal') , 
-						'post_name' => __('completed', 'pronamic_ideal') ,
-						'post_content' => __('<p>The payment process is successfully completed.</p>', 'pronamic_ideal')
+						'post_title'   => __( 'iDEAL payment completed', 'pronamic_ideal' ),
+						'post_name'    => __( 'completed', 'pronamic_ideal' ),
+						'post_content' => __( '<p>The payment process is successfully completed.</p>', 'pronamic_ideal' ),
+						'post_meta'    => array(
+							'_yoast_wpseo_meta-robots-noindex' =>true
+						)
 					)
 				)
 			)
 		);
 		
-		function pronamic_ideal_pages($pages, $namePrefix, $level = 0) {
+		function pronamic_ideal_pages( $posts, $name_prefix, $level = 0 ) {
 			?>
 			<ul style="padding-left: <?php echo $level * 25; ?>px">
 	
-				<?php foreach($pages as $i => $page): ?>
+				<?php foreach ( $posts as $i => $post ) : ?>
+		
+					<li>
+						<?php $name = $name_prefix . '[' . $i . ']'; ?>
 	
-				<li>
-					<?php $name = $namePrefix . '[' . $i . ']'; ?>
-
-					<h3><?php echo $page['post_title']; ?></h3>
-
-					<table class="form-table">
-						<tr>
-							<th scope="row">
-								<label for="pronamic_ideal_page_<?php echo $i; ?>_post_title">
-									<?php _e('Title', 'pronamic_ideal'); ?>
-								</label>
-							</th>
-							<td>
-				                <input id="pronamic_ideal_page_<?php echo $i; ?>_post_title" name="<?php echo $name; ?>[post_title]" value="<?php echo $page['post_title']; ?>" type="text" class="regular-text" />
-							</td>
-						</tr>
-						<tr>
-							<th scope="row">
-								<label for="pronamic_ideal_page_<?php echo $i; ?>_post_name">
-									<?php _e('Slug', 'pronamic_ideal'); ?>
-								</label>
-							</th>
-							<td>
-				                <input id="pronamic_ideal_page_<?php echo $i; ?>_post_name" name="<?php echo $name; ?>[post_name]" value="<?php echo $page['post_name']; ?>" type="text" class="regular-text" />
-							</td>
-						</tr>
-						<tr>
-							<th scope="row">
-								<label for="pronamic_ideal_page_<?php echo $i; ?>_post_content">
-									<?php _e('Content', 'pronamic_ideal'); ?>
-								</label>
-							</th>
-							<td>
-				                <textarea id="pronamic_ideal_page_<?php echo $i; ?>_post_content" name="<?php echo $name; ?>[post_content]" rows="2" cols="60"><?php echo $page['post_content']; ?></textarea>
-							</td>
-						</tr>
-					</table>
+						<h3><?php echo $post['post_title']; ?></h3>
 	
-					<?php 
-					
-					if(isset($page['children'])) {
-						pronamic_ideal_pages($page['children'], $name . '[children]', $level + 1);
-					} 
-					
-					?>				
-				</li>
+						<table class="form-table">
+							<tr>
+								<th scope="row">
+									<label for="pronamic_ideal_page_<?php echo $i; ?>_post_title">
+										<?php _e( 'Title', 'pronamic_ideal' ); ?>
+									</label>
+								</th>
+								<td>
+					                <input id="pronamic_ideal_page_<?php echo $i; ?>_post_title" name="<?php echo $name; ?>[post_title]" value="<?php echo $post['post_title']; ?>" type="text" class="regular-text" />
+								</td>
+							</tr>
+							<tr>
+								<th scope="row">
+									<label for="pronamic_ideal_page_<?php echo $i; ?>_post_name">
+										<?php _e( 'Slug', 'pronamic_ideal' ); ?>
+									</label>
+								</th>
+								<td>
+					                <input id="pronamic_ideal_page_<?php echo $i; ?>_post_name" name="<?php echo $name; ?>[post_name]" value="<?php echo $post['post_name']; ?>" type="text" class="regular-text" />
+								</td>
+							</tr>
+							<tr>
+								<th scope="row">
+									<label for="pronamic_ideal_page_<?php echo $i; ?>_post_content">
+										<?php _e( 'Content', 'pronamic_ideal' ); ?>
+									</label>
+								</th>
+								<td>
+					                <textarea id="pronamic_ideal_page_<?php echo $i; ?>_post_content" name="<?php echo $name; ?>[post_content]" rows="2" cols="60"><?php echo $post['post_content']; ?></textarea>
+								</td>
+							</tr>
+							<tr>
+								<th scope="row">
+									<?php _e( 'Meta', 'pronamic_ideal' ); ?>
+								</th>
+								<td>
+					                <?php if ( isset( $post['post_meta'] ) ) : ?>
+					                
+					                	<dl>
+					                		<?php foreach ( $post['post_meta'] as $key => $value ) : ?>
+					                		
+					                			<dt>
+					                				<?php echo $key; ?>
+					                			</dt>
+					                			<dd>
+					                				<input name="<?php echo $name; ?>[post_meta][<?php echo $key; ?>]" value="<?php echo $value; ?>" type="text" class="regular-text" />
+					                			</dd>
+					                		
+					                		<?php endforeach; ?>
+					                	</dl>
+	
+									<?php endif; ?>
+								</td>
+							</tr>
+						</table>
+		
+						<?php 
+						
+						if ( isset( $post['children'] ) ) {
+							pronamic_ideal_pages( $post['children'], $name . '[children]', $level + 1 );
+						}
+						
+						?>				
+					</li>
 			
 				<?php endforeach; ?>
 	
@@ -140,11 +187,11 @@ if(!empty($_POST) && check_admin_referer('pronamic_ideal_pages_generator', 'pron
 			<?php
 		}
 		
-		pronamic_ideal_pages($pages, 'pronamic_ideal_pages');
+		pronamic_ideal_pages( $pages, 'pronamic_ideal_pages' );
 
 		submit_button(
-			__('Generate Pages', 'pronamic_ideal') , 
-			'primary' ,
+			__( 'Generate Pages', 'pronamic_ideal' ),
+			'primary',
 			'create_pages'
 		);
 
