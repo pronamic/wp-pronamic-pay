078110136210b6f9c1fb386259e77b5cf7944ed5
Remco Tolsma <info@remcotolsma.nl>
2013-03-07 16:02:33 +0100

    Added class header documentation.

--- classes/Pronamic/S2Member/IDeal/AddOn.php
+++ classes/Pronamic/S2Member/IDeal/AddOn.php
@@ -1,8 +1,14 @@
 <?php
 
+/**
+ * Title: s2Member iDEAL add-on
+ * Description:
+ * Copyright: Copyright (c) 2005 - 2011
+ * Company: Pronamic
+ * @author Leon Rowland
+ * @since 1.2.6
+ */
 class Pronamic_S2Member_IDeal_AddOn {
-
-
     public static function bootstrap() {
         add_action( 'ws_plugin__s2member_loaded', array( __CLASS__, 'load' ) );
     }
