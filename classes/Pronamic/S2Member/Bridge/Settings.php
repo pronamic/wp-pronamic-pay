7d6d3126ce4bcb4e5e6d76543d33837410a8108e
Remco Tolsma <info@remcotolsma.nl>
2013-03-08 10:14:32 +0100

    WordPress Coding Standards optimizations.

--- classes/Pronamic/S2Member/Bridge/Settings.php
+++ classes/Pronamic/S2Member/Bridge/Settings.php
@@ -16,29 +16,29 @@ class Pronamic_S2Member_Bridge_Settings {
     }
 
     public function menu() {
-        $parent_slug = apply_filters("ws_plugin__s2member_during_add_admin_options_menu_slug", "ws-plugin--s2member-start" );
+        $parent_slug = apply_filters( 'ws_plugin__s2member_during_add_admin_options_menu_slug', 'ws-plugin--s2member-start' );
 
-        if(apply_filters("ws_plugin__s2member_during_add_admin_options_add_divider_6", true, get_defined_vars())) /* Divider. */
-            add_submenu_page($parent_slug, "", '<span style="display:block; margin:1px 0 1px -5px; padding:0; height:1px; line-height:1px; background:#CCCCCC;"></span>', "create_users", "#");
-
-        add_submenu_page(
-            $parent_slug,
-            __( 'Pronamic iDEAL Options', 'pronamic_ideal' ),
-            __( 'iDEAL Options', 'pronamic_ideal' ),
-            "create_users",
-            'pronamic-ideal-s2member-options',
-            array( $this, 'view_options_page' )
-        );
+        if ( apply_filters( 'ws_plugin__s2member_during_add_admin_options_add_divider_6', true, get_defined_vars() ) ) /* Divider. */
+			add_submenu_page( $parent_slug, '', '<span style="display:block; margin:1px 0 1px -5px; padding:0; height:1px; line-height:1px; background:#CCCCCC;"></span>', 'create_users', '#' );
 
 		add_submenu_page(
-            $parent_slug,
-            __( 'Pronamic iDEAL Buttons Generator', 'pronamic_ideal' ),
-            __( 'iDEAL Buttons', 'pronamic_ideal' ),
-            "create_users",
-            'pronamic-ideal-s2member-buttongen',
-            array( $this, 'view_buttongen_page' )
-        );
-    }
+			$parent_slug,
+			__( 'Pronamic iDEAL Options', 'pronamic_ideal' ),
+			__( 'iDEAL Options', 'pronamic_ideal' ),
+			'create_users',
+            'pronamic-ideal-s2member-options',
+            array( $this, 'view_options_page' )
+		);
+
+		add_submenu_page(
+			$parent_slug,
+			__( 'Pronamic iDEAL Buttons Generator', 'pronamic_ideal' ),
+			__( 'iDEAL Buttons', 'pronamic_ideal' ),
+			'create_users',
+			'pronamic-ideal-s2member-buttongen',
+			array( $this, 'view_buttongen_page' )
+		);
+	}
 
     public function view_options_page() {
         // Generate nonce field
@@ -81,14 +81,11 @@ class Pronamic_S2Member_Bridge_Settings {
                 </table>
                 <?php submit_button(); ?>
             </form>
-
 		</div>
         <?php
-
     }
 
     public function save_options_page() {
-
         if ( ! isset( $_POST['pronamic-ideal-s2member-options-nonce'] ) )
             return;
 
