9a441dc0fb91ca65479f7888f8b4f8cd28e94617
Remco Tolsma <info@remcotolsma.nl>
2013-04-01 17:30:44 +0200

    Improved documentation title.

--- classes/Pronamic/Gateways/Buckaroo/Parameters.php
+++ classes/Pronamic/Gateways/Buckaroo/Parameters.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Title: iDEAL Internet Kassa parameters constants
+ * Title: Buckaroo parameters constants
  * Description: 
  * Copyright: Copyright (c) 2005 - 2011
  * Company: Pronamic
