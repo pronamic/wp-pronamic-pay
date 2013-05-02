1a6d94a6446884b2cea8bab79783bd78d586f706
Leon Rowland <leon@rowland.nl>
2013-05-02 11:17:05 +0200

    addition of making an account and sending a mail if no account exists with that mail

--- classes/Pronamic/S2Member/IDeal/AddOn.php
+++ classes/Pronamic/S2Member/IDeal/AddOn.php
@@ -9,9 +9,10 @@
  * @since 1.2.6
  */
 class Pronamic_S2Member_IDeal_AddOn {
-    public static function bootstrap() {
-        add_action( 'plugins_loaded', array( __CLASS__, 'load' ) );
-    }
+
+	public static function bootstrap() {
+		add_action( 'plugins_loaded', array( __CLASS__, 'load' ) );
+	}
 
 	public static function load() {
 		if ( defined( 'WS_PLUGIN__S2MEMBER_VERSION' ) ) {
@@ -25,70 +26,83 @@ class Pronamic_S2Member_IDeal_AddOn {
 	}
 
 	public static function status_update( Pronamic_WordPress_IDeal_Payment $payment, $can_redirect = false ) {
-		if ( $payment->getSource() == 's2member' ) {
-			// Get the ID of the payment source ( which will be the meta id )
-			$user_id = Pronamic_S2Member_Bridge_Order::getUserFromUID( $payment->getSourceId() );
+		if ( $payment->getSource() == 's2member' && 'Success' === $payment->status ) {
 
-			// Get user order meta
-			$order_data = get_user_option( 'pronamic_ideal_s2member_order', $user_id );
+			$order_data[ 'orderID' ] = $payment->getSourceId();
+			$data					 = new Pronamic_S2Member_IDeal_IDealDataProxy( $order_data );
 
-			if ( 'completed' !== $order_data['status'] ) {
+			$url = $data->getNormalReturnUrl();
 
-				$order_data['orderID'] = $payment->getSourceId();
-				$data = new Pronamic_S2Member_IDeal_IDealDataProxy( $order_data );
+			switch ( $payment->status ) {
+				case Pronamic_Gateways_IDealAdvanced_Transaction::STATUS_CANCELLED:
+					$order_data[ 'status' ] = 'Cancelled';
 
-				$url = $data->getNormalReturnUrl();
+					$url = $data->getCancelUrl();
 
-				switch( $payment->status ) {
-					case Pronamic_Gateways_IDealAdvanced_Transaction::STATUS_CANCELLED:
-						$order_data['status'] = 'Cancelled';
+					break;
+				case Pronamic_Gateways_IDealAdvanced_Transaction::STATUS_EXPIRED:
+					$order_data[ 'status' ] = 'Expired';
 
-						$url = $data->getCancelUrl();
+					break;
+				case Pronamic_Gateways_IDealAdvanced_Transaction::STATUS_FAILURE:
+					$order_data[ 'status' ] = 'Failure';
 
-						break;
-					case Pronamic_Gateways_IDealAdvanced_Transaction::STATUS_EXPIRED:
-						$order_data['status'] = 'Expired';
+					break;
+				case Pronamic_Gateways_IDealAdvanced_Transaction::STATUS_SUCCESS:
+					// get account from email
+					$user = get_user_by( 'email', $payment->getEmail() );
 
-						break;
-					case Pronamic_Gateways_IDealAdvanced_Transaction::STATUS_FAILURE:
-						$order_data['status'] = 'Failure';
+					// No valid user?
+					if ( ! $user ) {
+						// Make a random string for password
+						$random_string = wp_generate_password( 10 );
 
-						break;
-					case Pronamic_Gateways_IDealAdvanced_Transaction::STATUS_SUCCESS:
-						// Add a registration time for their level
-						$registration_times = get_user_option( 's2member_paid_registration_times', $user_id );
+						// Make a user with the username as the email
+						$user_id = wp_create_user( $payment->getEmail(), $random_string, $payment->getEmail() );
+												
+						$subject = __( 'Account Confirmation', 'pronamic-ideal' ) . ' | ' . get_bloginfo( 'name' );
+						$message = sprintf( __( 'Your password is %s . Please change your password when you login', 'pronamic-ideal' ), $random_string );
+						wp_mail( $payment->getEmail(), $subject, $message );
+						
+					} else {
+						$user_id = $user->ID;
+					}
 
-						$ordered_level = $order_data['level'];
+					// Add a registration time for their level
+					$registration_times = get_user_option( 's2member_paid_registration_times', $user_id );
 
-						if ( empty( $registration_times ) ) $registration_times = array();
-						$registration_times['level' . $ordered_level] = time();
+					$ordered_level = $order_data[ 'level' ];
 
-						$user = new WP_User( $user_id );
-						// $user->add_cap( "s2member_level{$ordered_level}" );
-						$user->add_cap( "access_s2member_level{$ordered_level}" );
-						// $user->set_role( "s2member_level{$ordered_level}" );
+					if ( empty( $registration_times ) )
+						$registration_times = array( );
+					
+					$registration_times[ 'level' . $ordered_level ]	 = time();
 
-						update_user_option( $user_id, 's2member_paid_registration_times', $registration_times );
+					$user = new WP_User( $user_id );
+					// $user->add_cap( "s2member_level{$ordered_level}" );
+					$user->add_cap( "access_s2member_level{$ordered_level}" );
+					// $user->set_role( "s2member_level{$ordered_level}" );
 
-						$auto_time = c_ws_plugin__s2member_utils_time::auto_eot_time($user_id, $order_data['period'], false, false, $registration_times['level' . $ordered_level ] );
+					update_user_option( $user_id, 's2member_paid_registration_times', $registration_times );
 
-						update_user_option( $user_id, 's2member_auto_eot_time', $auto_time );
+					$auto_time = c_ws_plugin__s2member_utils_time::auto_eot_time( $user_id, $order_data[ 'period' ], false, false, $registration_times[ 'level' . $ordered_level ] );
 
-						break;
-					case Pronamic_Gateways_IDealAdvanced_Transaction::STATUS_OPEN:
-						$order_data['status'] = 'Open';
+					update_user_option( $user_id, 's2member_auto_eot_time', $auto_time );
 
-						break;
-					default:
-						$order_data['status'] = 'Unknown';
+					break;
+				case Pronamic_Gateways_IDealAdvanced_Transaction::STATUS_OPEN:
+					$order_data[ 'status' ] = 'Open';
 
-						break;
+					break;
+				default:
+					$order_data[ 'status' ] = 'Unknown';
+
+					break;
 
 					$order = new Pronamic_S2Member_Bridge_Order();
-					$order->update_order( $order_data, $user_id);
-
-				}
+					$order->update_order( $order_data, $user_id );
 			}
 		}
 	}
+
 }
