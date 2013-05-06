f4f9c4270d6d6a4808c2d9190ee7524a42f27cad
Remco Tolsma <info@remcotolsma.nl>
2013-05-06 11:57:25 +0200

    Adjusted the iDEAL Easy test page to the new gateway setup.

--- views/test-method-easy.php
+++ views/test-method-easy.php
@@ -1,3 +1,8 @@
+<?php 
+
+$gateway = new Pronamic_Gateways_IDealEasy_Gateway( $configuration );
+
+?>
 <h3>
 	<?php _e( 'Tests', 'pronamic_ideal' ); ?>
 </h3>
@@ -5,30 +10,19 @@
 <?php foreach ( array( 2, 3, 4, 5, 1 ) as $test_case ): ?>
 	
 	<?php 
-				
+
+	$data = new Pronamic_WordPress_IDeal_IDealTestDataProxy( wp_get_current_user(), $test_case );
+		
+	$gateway->start( $data );
+
 	$name = sprintf( __( 'Test &euro; %s', 'pronamic_ideal' ), $test_case );
-	
-	$user = wp_get_current_user();
-	
-	$iDeal = new Pronamic_Gateways_IDealEasy_IDealEasy();
-	
-	$iDeal->setPaymentServerUrl( $configuration->getPaymentServerUrl() );
-	$iDeal->setPspId( $configuration->pspId );
-	
-	$iDeal->setLanguage( Pronamic_WordPress_IDeal_Util::getLanguageIso639AndCountryIso3166Code() );
-	$iDeal->setCurrency( 'EUR' );
-	$iDeal->setOrderId( uniqid( 'test' ) );
-	$iDeal->setDescription( $name );
-	$iDeal->setAmount( $test_case );
-	$iDeal->setEMailAddress( $user->user_email );
-	$iDeal->setCustomerName( $user->user_firstname . ' ' . $user->user_lastname );
-	
+
 	?>
 	
-	<form method="post" action="<?php echo esc_attr( $iDeal->getPaymentServerUrl() ); ?>" target="_blank" style="display: inline">
+	<form method="post" action="<?php echo esc_attr( $gateway->get_action_url() ); ?>" target="_blank" style="display: inline">
 		<?php 
 	
-		echo $iDeal->getHtmlFields();
+		echo $gateway->get_output_html();
 	
 		submit_button( $name, 'secondary', 'submit', false ); 
 	
