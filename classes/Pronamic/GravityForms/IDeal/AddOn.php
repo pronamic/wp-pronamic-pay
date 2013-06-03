3cbd9aedf7bb4ca1ed2a120d6f972ccafd5403b5
Remco Tolsma <info@remcotolsma.nl>
2013-06-03 11:51:59 +0200

    Version 1.2.10.

--- classes/Pronamic/GravityForms/IDeal/AddOn.php
+++ classes/Pronamic/GravityForms/IDeal/AddOn.php
@@ -37,7 +37,7 @@ class Pronamic_GravityForms_IDeal_AddOn {
 	 * 
 	 * @var string
 	 */
-	const VERSION = '1.2.9';
+	const VERSION = '1.2.10';
 
 	//////////////////////////////////////////////////
 
