150c1379eb9d752a4384866a13983bf8f66db113
Remco Tolsma <info@remcotolsma.nl>
2013-06-05 08:32:29 +0200

    Fixed plugin name.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,7 +3,7 @@
 require_once getenv( 'WP_TESTS_DIR' ) . '/includes/functions.php';
 
 function _manually_load_plugin() {
-	require dirname( __FILE__ ) . '/../sample-plugin.php';
+	require dirname( __FILE__ ) . '/../pronamic-ideal.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
