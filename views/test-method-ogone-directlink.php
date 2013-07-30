c407ec43b8e6753270572449ec6d9a6e3dbdbad8
Remco Tolsma <info@remcotolsma.nl>
2013-07-30 13:40:11 +0200

    Adjusted some test info.

--- views/test-method-ogone-directlink.php
+++ views/test-method-ogone-directlink.php
@@ -10,16 +10,18 @@ $kassa->set_field( 'USERID', 'pronamic_api' );
 $kassa->set_field( 'PSWD', '' );
 $kassa->setAmount( 50 );
 $kassa->setCurrency( 'EUR' );
-$kassa->set_field( 'CARDNO', '4111111111111111' );
-$kassa->set_field( 'ED', '1218' );
-$kassa->setOrderDescription( 'Description' );
-$kassa->setCustomerName( 'Pronamic Test' );
-$kassa->setEMailAddress( 'test@pronamic.nl' );
+$kassa->set_field( 'CARDNO', '5555555555554444' );
+$kassa->set_field( 'ED', '01/15' );
+//$kassa->setOrderDescription( 'Description' );
+//$kassa->setCustomerName( 'Pronamic Test' );
+//$kassa->setEMailAddress( 'test@pronamic.nl' );
 $kassa->set_field( 'CVC', '000' );
 $kassa->set_field( 'OPERATION', 'SAL' );
 
 $data = $kassa->get_fields();
-$data['SHASign'] = $kassa->getSignatureIn();
+$data['SHASIGN'] = $kassa->getSignatureIn();
+
+//var_dump($data);
 
 $response = wp_remote_post( $url, array(
 	'method'  => 'POST',
