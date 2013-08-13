093aad294ee50b96841f1bb0bd2da4b4556ee0ab
Remco Tolsma <info@remcotolsma.nl>
2013-08-13 11:37:15 +0200

    Added Ogone DirectLink class.

--- classes/Pronamic/Gateways/Ogone/DirectLink.php
+++ classes/Pronamic/Gateways/Ogone/DirectLink.php
@@ -1,1 +1,27 @@
 <?php
+
+/**
+ * Title: Ogone DirectLink
+ * Description: 
+ * Copyright: Copyright (c) 2005 - 2011
+ * Company: Pronamic
+ * @author Remco Tolsma
+ * @version 1.0
+ */
+class Pronamic_Gateways_Ogone_DirectLink {
+	/**
+	 * Ogone DirectLink API endpoint URL
+	 * 
+	 * @var string
+	 */
+	const API_URL = 'https://secure.ogone.com/ncol/test/orderdirect.asp';
+
+	/////////////////////////////////////////////////
+
+	/**
+	 * Constructs and initializes an Ogone DirectLink object
+	 */
+	public function __construct() {
+		
+	}
+}
