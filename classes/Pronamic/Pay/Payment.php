d52810b09c5c2d182f60dae4d09c0775551e0521
Remco Tolsma <info@remcotolsma.nl>
2013-09-12 14:20:00 +0200

    Added some public vars to the payment class.

--- classes/Pronamic/Pay/Payment.php
+++ classes/Pronamic/Pay/Payment.php
@@ -2,4 +2,42 @@
 
 class Pronamic_Pay_Payment {
 
+
+	//////////////////////////////////////////////////
+	
+	public $purchase_id;
+	
+	public $transaction_id;
+	
+	public $amount;
+	
+	public $currency;
+	
+	public $expiration_period;
+	
+	public $language;
+	
+	public $entrance_code;
+	
+	public $description;
+	
+	public $consumer_name;
+	
+	public $consumer_account_number;
+	
+	public $consumer_iban;
+	
+	public $consumer_bic;
+	
+	public $consumer_city;
+	
+	public $status;
+	
+	public $status_requests;
+	
+	public $email;
+
+	//////////////////////////////////////////////////
+
+	
 }
