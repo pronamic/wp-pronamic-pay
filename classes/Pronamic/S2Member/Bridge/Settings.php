25f61e8c907f3dbc99379e5bc5017d5bc77575cb
Remco Tolsma <info@remcotolsma.nl>
2013-10-10 11:27:10 +0200

    Moved settings view to separate file.

--- classes/Pronamic/S2Member/Bridge/Settings.php
+++ classes/Pronamic/S2Member/Bridge/Settings.php
@@ -26,7 +26,7 @@ class Pronamic_S2Member_Bridge_Settings {
 			__( 'Pronamic iDEAL Options', 'pronamic_ideal' ),
 			__( 'iDEAL Options', 'pronamic_ideal' ),
 			'create_users',
-            'pronamic-ideal-s2member-options',
+            's2member_pronamic_ideal',
             array( $this, 'view_options_page' )
 		);
 
@@ -35,54 +35,13 @@ class Pronamic_S2Member_Bridge_Settings {
 			__( 'Pronamic iDEAL Buttons Generator', 'pronamic_ideal' ),
 			__( 'iDEAL Buttons', 'pronamic_ideal' ),
 			'create_users',
-			'pronamic-ideal-s2member-buttongen',
+			's2member_pronamic_ideal_buttons',
 			array( $this, 'view_buttongen_page' )
 		);
 	}
 
     public function view_options_page() {
-        // Generate nonce field
-        $nonce = wp_nonce_field( 'pronamic-ideal-s2member-options', 'pronamic-ideal-s2member-options-nonce', true, false );
-
-        // Get all configurations
-        $configurations = Pronamic_WordPress_IDeal_IDeal::get_configurations_select_options();
-
-        // Get existing options
-        $pronamic_ideal_s2member_enabled                = get_option( 'pronamic_ideal_s2member_enabled' );
-        $pronamic_ideal_s2member_chosen_configuration   = get_option( 'pronamic_ideal_s2member_chosen_configuration' );
-        ?>
-
-        <div class="wrap">
-            <?php screen_icon( 'pronamic_ideal' ); ?>
-            <h2><?php echo get_admin_page_title(); ?></h2>
-            <form action="" method="POST">
-                <?php echo $nonce; ?>
-                <table class="form-table">
-                    <tr>
-                        <th><?php _e( 'Enable/Disable', 'pronamic_ideal' ); ?></th>
-                        <td>
-                            <input type="checkbox" name="pronamic_ideal_s2member_enabled" value="1" <?php if ( $pronamic_ideal_s2member_enabled == 1 ) : ?> checked="checked" <?php endif;?> />
-                        </td>
-                    </tr>
-                    <tr>
-                        <th><?php _e( 'Configuration', 'pronamic_ideal' ); ?></th>
-                        <td>
-                            <select name="pronamic_ideal_s2member_chosen_configuration">
-                                <?php foreach ( $configurations as $value => $name ) : ?>
-                                    <?php if ( $value == $pronamic_ideal_s2member_chosen_configuration ) : ?>
-                                        <option value="<?php echo $value; ?>" selected="selected"><?php echo $name; ?></option>
-                                    <?php else: ?>
-                                        <option value="<?php echo $value; ?>"><?php echo $name; ?></option>
-                                    <?php endif;?>
-                                <?php endforeach; ?>
-                            </select>
-                        </td>
-                    </tr>
-                </table>
-                <?php submit_button(); ?>
-            </form>
-		</div>
-        <?php
+		return Pronamic_WordPress_IDeal_Admin::renderView( 's2member/settings' );
     }
 
     public function save_options_page() {
