aea5dabfcb7d1dbf485a02e75cdaf4dc8af6874c
Remco Tolsma <info@remcotolsma.nl>
2013-10-14 16:52:59 +0200

    Added basic dashboard.

--- views/dashboard.php
+++ views/dashboard.php
@@ -1,7 +1,19 @@
 <div class="wrap">
 	<?php screen_icon( 'pronamic_ideal' ); ?>
 
-	<h2>
-		<?php _e( 'Dashboard', 'pronamic_ideal' ); ?>
-	</h2>
+	<h2><?php echo get_admin_page_title(); ?></h2>
+
+	<div id="dashboard-widgets-wrap">
+		<div id="dashboard-widgets" class="metabox-holder columns-2">
+			<div id="postbox-container-1" class="postbox-container">
+				<div class="postbox">
+					<h3 class="hndle"><span><?php _e( 'Status', 'twinfield' ); ?></span></h3>
+
+					<div class="inside">
+						
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
