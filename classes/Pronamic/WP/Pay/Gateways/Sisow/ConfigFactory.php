de1269c9d13e478da376183ebf7b9c5b8ca6d4cd
Remco Tolsma <info@remcotolsma.nl>
2013-10-14 16:51:32 +0200

    Added some spacing.

--- classes/Pronamic/WP/Pay/Gateways/Sisow/ConfigFactory.php
+++ classes/Pronamic/WP/Pay/Gateways/Sisow/ConfigFactory.php
@@ -5,6 +5,7 @@ class Pronamic_WP_Pay_Gateways_Sisow_ConfigFactory {
 		$config = new Pronamic_Gateways_Sisow_Config();
 
 		$config->mode         = get_post_meta( $post_id, '_pronamic_gateway_mode', true );
+
 		$config->merchant_id  = get_post_meta( $post_id, '_pronamic_gateway_sisow_merchant_id', true );
 		$config->merchant_key = get_post_meta( $post_id, '_pronamic_gateway_sisow_merchant_key', true );
 		
