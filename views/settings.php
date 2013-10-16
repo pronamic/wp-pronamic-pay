525b914b3d59dba414ee4a36922c8412a1138237
Remco Tolsma <info@remcotolsma.nl>
2013-10-16 14:53:51 +0200

    Use the WordPress get admin page title.

--- views/settings.php
+++ views/settings.php
@@ -6,9 +6,7 @@ global $wpdb;
 <div class="wrap">
 	<?php screen_icon( 'pronamic_ideal' ); ?>
 
-	<h2>
-		<?php _e( 'iDEAL Settings', 'pronamic_ideal' ); ?>
-	</h2>
+	<h2><?php echo get_admin_page_title(); ?></h2>
 
 	<form action="options.php" method="post">
 		<?php settings_fields( 'pronamic_pay' ); ?>
