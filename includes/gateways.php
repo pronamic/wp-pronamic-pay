9380282eeb2be40849033e262fbbb64a357ce6ec
Remco Tolsma <info@remcotolsma.nl>
2013-10-18 21:50:29 +0200

    Fixed refenrecens to iDEAL simulator v3.

--- includes/gateways.php
+++ includes/gateways.php
@@ -292,10 +292,10 @@ $pronamic_pay_gateways = array(
 			'payment_server_url' => 'https://www.ideal-simulator.nl/professional-v3/'
 		),
 		'live'     => array(
-			'payment_server_url' => 'https://www.ideal-simulator.nl/professional-v3/'
+			'payment_server_url' => 'https://www.ideal-simulator.nl/professional-v3//'
 		),
 		'certificates' => array(
-			'certificates/ideal-simulator-ideal-advanced-v3/simulator.cer'
+			'certificates/ideal-simulator-ideal-advanced-v3/simulator-2048.cer'
 		)
 	),
 	// ING
