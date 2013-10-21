f7b82d13ede9f32e6c7ae093867b52a4884b9c3a
Remco Tolsma <info@remcotolsma.nl>
2013-10-21 09:45:02 +0200

    Improved support for eShop.

--- classes/Pronamic/EShop/IDeal/AddOn.php
+++ classes/Pronamic/EShop/IDeal/AddOn.php
@@ -25,30 +25,32 @@ class Pronamic_EShop_IDeal_AddOn {
 		/**
 		 * WordPress
 		 */
-		add_action('init',  array(__CLASS__, 'initialize'), 20);
+		add_action( 'init',  array( __CLASS__, 'init' ), 20 );
 
 		/**
 		 * Pronamic iDEAL filters
 		 */
-		add_filter('pronamic_payment_source_text_eshop', array(__CLASS__, 'sourceColumn'), 10, 2);
+		add_filter( 'pronamic_payment_source_text_eshop', array( __CLASS__, 'sourceColumn' ), 10, 2 );
 		
 		/**
 		 * eShop actions
+		 * 
+		 * @see http://plugins.trac.wordpress.org/browser/eshop/tags/6.2.12/eshop-settings-extends.php#L1049
 		 */
-		// @see http://plugins.trac.wordpress.org/browser/eshop/tags/6.2.12/eshop-settings-extends.php#L1049
-		add_action('eshop_setting_merchant_load', array(__CLASS__, 'loadMerchantSettings'));
+		add_action( 'eshop_setting_merchant_load', array( __CLASS__, 'setting_merchant_load' ) );
 
 		/**
 		 * eShop filter
+		 * 
+		 * @see http://plugins.trac.wordpress.org/browser/eshop/tags/6.2.12/eshop-settings-extends.php#L1460
 		 */
-		// @see http://plugins.trac.wordpress.org/browser/eshop/tags/6.2.12/eshop-settings-extends.php#L1460
-		add_filter('eshop_setting_merchant_save', array(__CLASS__, 'saveMerchantSettings'), 20, 2);
+		add_filter( 'eshop_setting_merchant_save', array( __CLASS__, 'setting_merchant_save' ), 20, 2 );
 
 		// @see http://plugins.trac.wordpress.org/browser/eshop/tags/6.2.12/eshop-settings-extends.php#L1365
 		// @see http://plugins.trac.wordpress.org/browser/eshop/tags/6.2.12/checkout.php#L315
 		$eshoppayment = 'pronamic_ideal';
 
-		add_filter('eshop_merchant_img_' . $eshoppayment, array(__CLASS__,  'merchantImage'));
+		add_filter( 'eshop_merchant_img_' . $eshoppayment, array( __CLASS__,  'merchant_img' ) );
 	}
 
 	//////////////////////////////////////////////////
@@ -56,7 +58,7 @@ class Pronamic_EShop_IDeal_AddOn {
 	/**
 	 * Initialize
 	 */
-	public static function initialize() {
+	public static function init() {
 		
 	}
 
@@ -65,13 +67,13 @@ class Pronamic_EShop_IDeal_AddOn {
 	/**
 	 * Load merchant settings
 	 */
-	public static function loadMerchantSettings($metaBox) {
+	public static function setting_merchant_load( $meta_box ) {
 		add_meta_box(
-			'eshop-m-pronamic_ideal' , 
-			__('Pronamic iDEAL', 'pronamic_ideal') , 
-			array(__CLASS__, 'metaBox') , 
-			$metaBox->pagehook , 
-			'normal' , 
+			'eshop-m-pronamic_ideal',
+			__( 'Pronamic iDEAL', 'pronamic_ideal' ), 
+			array( __CLASS__, 'meta_box' ),
+			$meta_box->pagehook,
+			'normal',
 			'core'
 		);
 	}
@@ -79,14 +81,20 @@ class Pronamic_EShop_IDeal_AddOn {
 	/**
 	 * Save merchant settings
 	 * 
-	 * @param array $eShopOptions
-	 * @param array $postData
+	 * @param array $eshopoptions
+	 * @param array $post_data 
 	 */
-	public static function saveMerchantSettings($eShopOptions, $postData) {
+	public static function setting_merchant_save( $eshopoptions, $post_data ) {
 		// We should move the post data to the options, eShop will take care of the actual saving
+		if ( isset( $post_data['eshop_pronamic_ideal_config_id'] ) ) {
+			$options = array(
+				'config_id' => filter_var( $post_data['eshop_pronamic_ideal_config_id'], FILTER_VALIDATE_INT )
+			);
 
+			$eshopoptions['pronamic_ideal'] = $options;
+		}
 
-		return $eShopOptions;
+		return $eshopoptions;
 	}
 
 	//////////////////////////////////////////////////
@@ -98,11 +106,11 @@ class Pronamic_EShop_IDeal_AddOn {
 	 * @see http://plugins.trac.wordpress.org/browser/eshop/tags/6.2.12/eshop-settings-extends.php#L1365
 	 * @see http://plugins.trac.wordpress.org/browser/eshop/tags/6.2.12/checkout.php#L315
 	 */
-	public static function merchantImage($image) {
+	public static function merchant_img( $image ) {
 		$relative = 'images/ideal-logo-pay-off-2-lines.png';
 
-		$image['path'] = plugin_dir_path(Pronamic_WordPress_IDeal_Plugin::$file) . $relative;
-		$image['url'] = plugins_url($relative, Pronamic_WordPress_IDeal_Plugin::$file);
+		$image['path'] = plugin_dir_path( Pronamic_WordPress_IDeal_Plugin::$file ) . $relative;
+		$image['url']  = plugins_url( $relative, Pronamic_WordPress_IDeal_Plugin::$file );
 
 		return $image;
 	}
@@ -112,47 +120,8 @@ class Pronamic_EShop_IDeal_AddOn {
 	/**
 	 * Meta box
 	 */
-	public static function metaBox() {
-		// Globals
-		global $eshop_metabox_plugin, $eshopoptions;
-
-		// eShop options
-		$config_id = @$eshopoptions['pronamic_ideal'];
-		
-		$configs = Pronamic_WordPress_IDeal_IDeal::get_config_select_options();
-
-		?>
-		<fieldset>
-			<?php $eshop_metabox_plugin->show_img('pronamic_ideal'); ?>
-
-			<p class="cbox">
-				<input id="eshop_method_pronamic_ideal" name="eshop_method[]" type="checkbox" value="pronamic_ideal" <?php checked(in_array('pronamic_ideal', (array) $eshopoptions['method'])); ?> />
-
-				<label for="eshop_method_pronamic_ideal" class="eshopmethod">
-					<?php _e( 'Accept payment by iDEAL', 'pronamic_ideal' ); ?>
-				</label>
-			</p>
-
-			<label for="eshop_pronamic_ideal_config_id">
-				<?php _e( 'Configuration', 'pronamic_ideal' ) ?>:
-			</label>
-
-			<select name="eshop_pronamic_ideal_config_id" id="eshop_pronamic_ideal_config_id">
-				<?php
-				
-				foreach ( $configs as $id => $name ) {
-					printf(
-						'<option value="%s" %s>%s</option>',
-						esc_attr( $id ),
-						selected( $id, $config_id, false ),
-						esc_html( $name )
-					);
-				}
-
-				?>
-			</select>
-		</fieldset>
-		<?php
+	public static function meta_box() {
+		include Pronamic_WordPress_IDeal_Plugin::$dirname . '/views/eshop/settings-meta-box.php';
 	}
 
 	//////////////////////////////////////////////////
@@ -160,7 +129,7 @@ class Pronamic_EShop_IDeal_AddOn {
 	/**
 	 * Source column
 	 */
-	public static function sourceColumn($text, $payment) {
+	public static function sourceColumn( $text, $payment ) {
 		$text  = '';
 		$text .= __( 'eShop', 'pronamic_ideal' ) . '<br />';
 		$text .= sprintf('<a href="%s">', add_query_arg( array( 'page' => 'gf_pronamic_ideal', 'lid' => $payment->getSourceId() ), admin_url( 'admin.php' ) ) );
---
+++ views/eshop/settings-meta-box.php
@@ -0,0 +1,29 @@
+<?php 
+
+global $eshop_metabox_plugin, $eshopoptions;
+
+?>
+<fieldset>
+	<?php $eshop_metabox_plugin->show_img( 'pronamic_ideal' ); ?>
+
+	<p class="cbox">
+		<input id="eshop_method_pronamic_ideal" name="eshop_method[]" type="checkbox" value="pronamic_ideal" <?php checked( in_array( 'pronamic_ideal', (array) $eshopoptions['method'] ) ); ?> />
+
+		<label for="eshop_method_pronamic_ideal" class="eshopmethod">
+			<?php _e( 'Accept payment by iDEAL', 'pronamic_ideal' ); ?>
+		</label>
+	</p>
+
+	<label for="eshop_pronamic_ideal_config_id">
+		<?php _e( 'Configuration', 'pronamic_ideal' ); ?>
+	</label>
+
+	<?php 
+	
+	Pronamic_WordPress_IDeal_Admin::dropdown_configs( array(
+		'name'     => 'eshop_pronamic_ideal_config_id',
+		'selected' => @$eshopoptions['pronamic_ideal']['config_id']
+	) );
+	
+	?>
+</fieldset>
