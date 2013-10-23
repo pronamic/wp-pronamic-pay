c88412d997f7399a0ca77891eee8008ed8011794
Remco Tolsma <info@remcotolsma.nl>
2013-10-23 13:30:24 +0200

    Update the uninstall file to remove everything.

--- uninstall.php
+++ uninstall.php
@@ -2,18 +2,72 @@
 
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) )
 	exit ();
-	
-// Delete tables
+
 global $wpdb;
+
+//////////////////////////////////////////////////
+// Delete tables
+//////////////////////////////////////////////////
 
 $wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}pronamic_ideal_configurations" );
 $wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}pronamic_ideal_payments" );
 $wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}rg_ideal_feeds" );
 
-// Delete options
+//////////////////////////////////////////////////
+// Delete posts
+//////////////////////////////////////////////////
+
+$wpdb->query( "DELETE FROM wp_posts WHERE post_type = 'pronamic_gateway';" );
+$wpdb->query( "DELETE FROM wp_posts WHERE post_type = 'pronamic_payment';" );
+$wpdb->query( "DELETE FROM wp_posts WHERE post_type = 'pronamic_pay_gf';" );
+
+$wpdb->query( "DELETE FROM wp_postmeta WHERE post_id NOT IN ( SELECT ID FROM wp_posts );" );
+
+//////////////////////////////////////////////////
+// Delete general options
+//////////////////////////////////////////////////
+
+
+
+//////////////////////////////////////////////////
+// Delete exntesion options
+//////////////////////////////////////////////////
+
+// Event Espresso 
+delete_option( '' );
+
+// Gravity Forms
+// There are options for Gravity Forms
+
+// Jigoshop 
+delete_option( '' );
+
+// Membership 
+delete_option( '' );
+
+// s2Member® 
+delete_option( '' );
+
+// Shopp
+// Shopp options are stored in the Shopp meta table, we don't touch this
+
+// WooCommerce
+delete_option( '' );
+
+// WP e-Commerce
+delete_option( '' );
+
+// ClassiPress
+delete_option( '' );
+
+// JobRoller
+delete_option( '' );
+
+//////////////////////////////////////////////////
+// Delete legacy options
+//////////////////////////////////////////////////
+
 delete_option( 'pronamic_ideal_version' );
+delete_option( 'pronamic_ideal_db_version' );
 delete_option( 'pronamic_ideal_key' );
 delete_option( 'gf_ideal_version' );
-
-// Delete transient
-delete_transient( 'pronamic_ideal_license_info' );
