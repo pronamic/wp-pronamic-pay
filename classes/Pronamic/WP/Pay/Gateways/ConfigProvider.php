1d3c37f2777dd5e9d9c169771ea6fce0008b42e5
Remco Tolsma <info@remcotolsma.nl>
2013-10-28 14:20:21 +0100

    Fixing issue on PHP 5.2.
    Parse error: syntax error, unexpected T_PAAMAYIM_NEKUDOTAYIM in /home/travis/build/pronamic/wp-pronamic-ideal/classes/Pronamic/WP/Pay/Gateways/ConfigProvider.php on line 17

--- classes/Pronamic/WP/Pay/Gateways/ConfigProvider.php
+++ classes/Pronamic/WP/Pay/Gateways/ConfigProvider.php
@@ -14,7 +14,12 @@ class Pronamic_WP_Pay_Gateways_ConfigProvider {
 			$class_name = self::$factories[$name];
 
 			if ( class_exists( $class_name ) ) {
-				$config = $class_name::get_config( $post_id );
+				// Fix - Parse error: syntax error, unexpected T_PAAMAYIM_NEKUDOTAYIM
+				if ( version_compare( PHP_VERSION, '5.3', '>=' ) ) {
+					$config = $class_name::get_config( $post_id );
+				} else {
+					$config = call_user_func( array( $class_name, 'get_config' ), $post_id );
+				}
 			}
 		}
 
