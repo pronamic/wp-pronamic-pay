b98c9e9f1e6875a9f7f062366884c62f4664c1e5
Remco Tolsma <info@remcotolsma.nl>
2013-11-21 20:28:50 +0100

    Simplified cURL version.

--- views/status-system.php
+++ views/status-system.php
@@ -410,24 +410,9 @@
 						<?php 
 						
 						$version = curl_version(); 
-						
-						$elements = array(
-							// 'version_number'     => 'cURL 24 bit version number',
-							'version'            => __( 'cURL version', 'pronamic_ideal' ), // cURL version number, as a string
-							// 'ssl_version_number' => 'OpenSSL 24 bit version number',
-							'ssl_version'        => __( 'OpenSSL version', 'pronamic_ideal' ), // OpenSSL version number, as a string
-							'libz_version'       => __( 'Zlib version', 'pronamic_ideal' ), // zlib version number, as a string
-							'host'               => __( 'Host cURL built', 'pronamic_ideal' ), // Information about the host where cURL was built
-							'age'                => __( 'Age', 'pronamic_ideal' ),
-							// 'features'           => 'A bitmask of the CURL_VERSION_XXX constants',
-							// 'protocols'          => __( 'Protocols', 'pronamic_ideal' ), // An array of protocols names supported by cURL
-						);
 
-						foreach ( $elements as $indice => $description ) {
-							if ( isset ( $version[$indice] ) ) {
-								printf( '<dt>%s</dt>', $description );
-								printf( '<dd>%s</dd>', $version[$indice] );
-							}
+						if ( isset( $version['version'] ) ) {
+							echo $version['version'];
 						}
 						
 						?>
