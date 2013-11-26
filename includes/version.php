5536ef19c78419ab04a3dcc00aeaeeb8199199a8
Remco Tolsma <info@remcotolsma.nl>
2013-11-26 10:57:29 +0100

    Updated to version 2.2.2.

--- includes/version.php
+++ includes/version.php
@@ -6,7 +6,7 @@
  */
 global $pronamic_pay_version;
 
-$pronamic_pay_version = '2.2.1';
+$pronamic_pay_version = '2.2.2';
 
 /**
  * Holds the Pronamic Pay DB revision, increments when changes are made to the Pronamic Pay DB schema.
@@ -15,4 +15,4 @@ $pronamic_pay_version = '2.2.1';
  */
 global $pronamic_pay_db_version;
 
-$pronamic_pay_db_version = 221;
+$pronamic_pay_db_version = 222;
