8370b9425d7438bbe103e2966b3c58a7d037e101
Remco Tolsma <info@remcotolsma.nl>
2013-12-12 09:54:07 +0100

    Cleaning up test credit card number.

--- classes/Pronamic/WP/Pay/PaymentTestData.php
+++ classes/Pronamic/WP/Pay/PaymentTestData.php
@@ -129,16 +129,18 @@ class Pronamic_WP_Pay_PaymentTestData extends Pronamic_WP_Pay_PaymentData {
 		$credit_card = new Pronamic_Pay_CreditCard();
 
 		// @see http://www.paypalobjects.com/en_US/vhelp/paypalmanager_help/credit_card_numbers.htm
-		// $credit_card->set_number( '5555555555554444' );
 		// Test card to simulate a 3-D Secure registered card
-		$credit_card->set_number( '4000 0000 0000 0002' );
-		$credit_card->set_number( '4111111111111111' );
-		$credit_card->set_number( '4000000000000002' );
-		//$credit_card->set_number( '4000 0000 0000 0002' );
+		// $credit_card->set_number( '5555555555554444' );
+		// $credit_card->set_number( '4111111111111111' );
+		// $credit_card->set_number( '4000000000000002' );
 		$credit_card->set_number( '5300000000000006' );
+
 		$credit_card->set_expiration_month( 12 );
+
 		$credit_card->set_expiration_year( date( 'Y' ) + 5 );
+
 		$credit_card->set_security_code( '123' );
+
 		$credit_card->set_name( 'Pronamic' );
 
 		return $credit_card;
