6316fe3e51cf8ce9b9bb1c49ceb88e7f1a2123c9
Remco Tolsma <info@remcotolsma.nl>
2013-12-12 10:50:22 +0100

    Fixed notice of $config var usage, should be $config_id.

--- classes/Pronamic/AppThemes/IDeal/IDealGateway.php
+++ classes/Pronamic/AppThemes/IDeal/IDealGateway.php
@@ -58,7 +58,7 @@ class Pronamic_AppThemes_IDeal_IDealGateway extends APP_Gateway {
 				$data = new Pronamic_WP_Pay_AppThemes_PaymentData( $order );
 
 				if ( filter_has_var( INPUT_POST, 'classipress_pronamic_ideal' ) ) {
-					$payment = Pronamic_WordPress_IDeal_IDeal::start( $config, $gateway, $data );
+					$payment = Pronamic_WordPress_IDeal_IDeal::start( $config_id, $gateway, $data );
 					
 					$error = $gateway->get_error();
 					
