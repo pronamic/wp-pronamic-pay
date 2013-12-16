3cf64dc24054602cf5cf3687762bec0ae8d2ccb4
Remco Tolsma <info@remcotolsma.nl>
2013-12-16 14:45:28 +0100

    Make sure we also store the meta stored in the payment meta array.

--- includes/payment.php
+++ includes/payment.php
@@ -6,7 +6,7 @@ function pronamic_wp_pay_update_payment( Pronamic_WP_Pay_Payment $payment ) {
 	// Meta
 	$prefix = '_pronamic_payment_';
 	
-	$meta = array(
+	$meta = $payment->meta + array(
 		'transaction_id'          => $payment->get_transaction_id(),
 		'action_url'              => $payment->get_action_url(),
 		'status'                  => $payment->status,
