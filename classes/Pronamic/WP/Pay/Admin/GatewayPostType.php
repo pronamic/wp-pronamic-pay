0f53ffece1d8e04b8dd4ddb0b0b5bd8f7cc9383c
Remco Tolsma <info@remcotolsma.nl>
2014-02-07 11:12:42 +0100

    Also move the save_post actions to the admin class to make sure they are only hooked in on the admin side.

--- classes/Pronamic/WP/Pay/Admin/GatewayPostType.php
+++ classes/Pronamic/WP/Pay/Admin/GatewayPostType.php
@@ -17,6 +17,8 @@ class Pronamic_WP_Pay_Admin_GatewayPostType {
 		add_action( 'post_edit_form_tag', array( $this, 'post_edit_form_tag' ) );
 
 		add_action( 'add_meta_boxes', array( $this, 'add_meta_boxes' ) );
+		
+		add_action( 'save_post', array( $this, 'save_post' ) );
 	}
 
 	public function edit_columns( $columns ) {
@@ -170,7 +172,7 @@ class Pronamic_WP_Pay_Admin_GatewayPostType {
 	 *
 	 * @param WP_Post $post The object for the current post/page.
 	 */
-	function meta_box_config( $post ) {
+	public function meta_box_config( $post ) {
 		include Pronamic_WordPress_IDeal_Plugin::$dirname . '/views/config-edit.php';
 	}
 	
@@ -179,7 +181,125 @@ class Pronamic_WP_Pay_Admin_GatewayPostType {
 	 *
 	 * @param WP_Post $post The object for the current post/page.
 	 */
-	function meta_box_test( $post ) {
+	public function meta_box_test( $post ) {
 		include Pronamic_WordPress_IDeal_Plugin::$dirname . '/views/config-test.php';
+	}
+		
+	/**
+	 * When the post is saved, saves our custom data.
+	 *
+	 * @param int $post_id The ID of the post being saved.
+	 */
+	public function pronamic_pay_save_gateway( $post_id ) {
+		// Check if our nonce is set.
+		if ( ! filter_has_var( INPUT_POST, 'pronamic_pay_nonce' ) )
+			return $post_id;
+	
+		$nonce = filter_input( INPUT_POST, 'pronamic_pay_nonce', FILTER_SANITIZE_STRING );
+	
+		// Verify that the nonce is valid.
+		if ( ! wp_verify_nonce( $nonce, 'pronamic_pay_save_gateway' ) )
+			return $post_id;
+	
+		// If this is an autosave, our form has not been submitted, so we don't want to do anything.
+		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE )
+			return $post_id;
+	
+		// Check the user's permissions.
+		if ( 'page' == $_POST['post_type'] ) {
+	
+			if ( ! current_user_can( 'edit_page', $post_id ) )
+				return $post_id;
+	
+		} else {
+	
+			if ( ! current_user_can( 'edit_post', $post_id ) )
+				return $post_id;
+		}
+	
+		/* OK, its safe for us to save the data now. */
+		$definition = array(
+			// General
+			'_pronamic_gateway_id'   => FILTER_SANITIZE_STRING,
+			'_pronamic_gateway_mode' => FILTER_SANITIZE_STRING,
+			// Basic
+			'_pronamic_gateway_ideal_merchant_id' => FILTER_SANITIZE_STRING,
+			'_pronamic_gateway_ideal_sub_id'      => FILTER_SANITIZE_STRING,
+			'_pronamic_gateway_ideal_hash_key'    => FILTER_SANITIZE_STRING,
+			// PayDutch
+			'_pronamic_gateway_paydutch_username' => FILTER_SANITIZE_STRING,
+			'_pronamic_gateway_paydutch_password' => FILTER_SANITIZE_STRING,
+			// Mollie
+			'_pronamic_gateway_mollie_api_key'     => FILTER_SANITIZE_STRING,
+			'_pronamic_gateway_mollie_partner_id'  => FILTER_SANITIZE_STRING,
+			'_pronamic_gateway_mollie_profile_key' => FILTER_SANITIZE_STRING,
+			// MultiSafepay
+			'_pronamic_gateway_multisafepay_account_id' => FILTER_SANITIZE_STRING,
+			'_pronamic_gateway_multisafepay_site_id'    => FILTER_SANITIZE_STRING,
+			'_pronamic_gateway_multisafepay_site_code'  => FILTER_SANITIZE_STRING,
+			// OmniKassa
+			'_pronamic_gateway_omnikassa_merchant_id' => FILTER_SANITIZE_STRING,
+			'_pronamic_gateway_omnikassa_secret_key'  => FILTER_SANITIZE_STRING,
+			'_pronamic_gateway_omnikassa_key_version' => FILTER_SANITIZE_STRING,
+			// Buckaroo
+			'_pronamic_gateway_buckaroo_website_key' => FILTER_SANITIZE_STRING,
+			'_pronamic_gateway_buckaroo_secret_key'  => FILTER_SANITIZE_STRING,
+			// ICEPAY
+			'_pronamic_gateway_icepay_merchant_id' => FILTER_SANITIZE_STRING,
+			'_pronamic_gateway_icepay_secret_code' => FILTER_SANITIZE_STRING,
+			// Sisow
+			'_pronamic_gateway_sisow_merchant_id'  => FILTER_SANITIZE_STRING,
+			'_pronamic_gateway_sisow_merchant_key' => FILTER_SANITIZE_STRING,
+			// TargetPay
+			'_pronamic_gateway_targetpay_layoutcode' => FILTER_SANITIZE_STRING,
+			// Ogone
+			'_pronamic_gateway_ogone_psp_id'              => FILTER_SANITIZE_STRING,
+			'_pronamic_gateway_ogone_hash_algorithm'      => FILTER_SANITIZE_STRING,
+			'_pronamic_gateway_ogone_sha_in_pass_phrase'  => FILTER_SANITIZE_STRING,
+			'_pronamic_gateway_ogone_sha_out_pass_phrase' => FILTER_SANITIZE_STRING,
+			'_pronamic_gateway_ogone_user_id'             => FILTER_SANITIZE_STRING,
+			'_pronamic_gateway_ogone_password'            => FILTER_SANITIZE_STRING,
+			// Ogone DirectLink
+			'_pronamic_gateway_ogone_directlink_sha_in_pass_phrase' => FILTER_SANITIZE_STRING,
+			'_pronamic_gateway_ogone_3d_secure_enabled' => FILTER_VALIDATE_BOOLEAN,
+			// Qantani
+			'_pronamic_gateway_qantani_merchant_id'     => FILTER_SANITIZE_STRING,
+			'_pronamic_gateway_qantani_merchant_secret' => FILTER_SANITIZE_STRING,
+			'_pronamic_gateway_qantani_merchant_key'    => FILTER_SANITIZE_STRING,
+			// Advanced
+			'_pronamic_gateway_ideal_private_key_password' => FILTER_SANITIZE_STRING,
+			'_pronamic_gateway_ideal_private_key'          => FILTER_SANITIZE_STRING,
+			'_pronamic_gateway_ideal_private_certificate'  => FILTER_SANITIZE_STRING,
+			'_pronamic_gateway_number_days_valid'          => FILTER_SANITIZE_STRING,
+			'_pronamic_gateway_country'                    => FILTER_SANITIZE_STRING,
+			'_pronamic_gateway_state_or_province'          => FILTER_SANITIZE_STRING,
+			'_pronamic_gateway_locality'                   => FILTER_SANITIZE_STRING,
+			'_pronamic_gateway_organization'               => FILTER_SANITIZE_STRING,
+			'_pronamic_gateway_organization_unit'          => FILTER_SANITIZE_STRING,
+			'_pronamic_gateway_common_name'                => FILTER_SANITIZE_STRING,
+			'_pronamic_gateway_email'                      => FILTER_SANITIZE_STRING
+		);
+		
+		$data = filter_input_array( INPUT_POST, $definition );
+	
+		// Files
+		$files = array(
+			'_pronamic_gateway_ideal_private_key_file'         => '_pronamic_gateway_ideal_private_key',
+			'_pronamic_gateway_ideal_private_certificate_file' => '_pronamic_gateway_ideal_private_certificate'
+		);
+	
+		foreach ( $files as $name => $meta_key ) {
+			if ( isset( $_FILES[ $name ] ) && $_FILES[ $name ]['error'] == UPLOAD_ERR_OK ) {
+				$value = file_get_contents( $_FILES[ $name ]['tmp_name'] );
+				
+				$data[$meta_key] = $value;
+			}
+		}
+	
+		// Update post meta data
+		pronamic_pay_update_post_meta_data( $post_id, $data );
+	
+		// Transient
+		delete_transient( 'pronamic_pay_issuers_' . $post_id );
 	}
 }
--- classes/Pronamic/WP/Pay/Admin/PaymentFormPostType.php
+++ classes/Pronamic/WP/Pay/Admin/PaymentFormPostType.php
@@ -15,6 +15,8 @@ class Pronamic_WP_Pay_Admin_PaymentFormPostType {
 		add_action( 'manage_pronamic_pay_gf_posts_custom_column', array( $this, 'custom_columns' ), 10, 2 );
 
 		add_action( 'add_meta_boxes', array( $this, 'add_meta_boxes' ) );
+		
+		add_action( 'save_post', array( $this, 'save_post' ) );
 	}
 	
 	public function edit_columns( $columns ) {
@@ -89,5 +91,72 @@ class Pronamic_WP_Pay_Admin_PaymentFormPostType {
 	 */
 	public function meta_box_config( $post ) {
 		include Pronamic_WordPress_IDeal_Plugin::$dirname . '/views/gravityforms/feed-edit.php';
+	}
+
+	/**
+	 * When the post is saved, saves our custom data.
+	 *
+	 * @param int $post_id The ID of the post being saved.
+	 */
+	public function save_post( $post_id ) {
+		// Check if our nonce is set.
+		if ( ! filter_has_var( INPUT_POST, 'pronamic_pay_nonce' ) )
+			return $post_id;
+	
+		$nonce = filter_input( INPUT_POST, 'pronamic_pay_nonce', FILTER_SANITIZE_STRING );
+	
+		// Verify that the nonce is valid.
+		if ( ! wp_verify_nonce( $nonce, 'pronamic_pay_save_pay_gf' ) )
+			return $post_id;
+	
+		// If this is an autosave, our form has not been submitted, so we don't want to do anything.
+		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE )
+			return $post_id;
+	
+		// Check the user's permissions.
+		if ( 'page' == $_POST['post_type'] ) {
+	
+			if ( ! current_user_can( 'edit_page', $post_id ) )
+				return $post_id;
+	
+		} else {
+	
+			if ( ! current_user_can( 'edit_post', $post_id ) )
+				return $post_id;
+		}
+	
+		/* OK, its safe for us to save the data now. */
+		$definition = array(
+				'_pronamic_pay_gf_form_id'                            => FILTER_SANITIZE_STRING,
+				'_pronamic_pay_gf_config_id'                          => FILTER_SANITIZE_STRING,
+				'_pronamic_pay_gf_entry_id_prefix'                    => FILTER_SANITIZE_STRING,
+				'_pronamic_pay_gf_transaction_description'            => FILTER_SANITIZE_STRING,
+				'_pronamic_pay_gf_condition_enabled'                  => FILTER_VALIDATE_BOOLEAN,
+				'_pronamic_pay_gf_condition_field_id'                 => FILTER_SANITIZE_STRING,
+				'_pronamic_pay_gf_condition_operator'                 => FILTER_SANITIZE_STRING,
+				'_pronamic_pay_gf_condition_value'                    => FILTER_SANITIZE_STRING,
+				'_pronamic_pay_gf_delay_notification_ids'             => array(
+						'filter'    => FILTER_SANITIZE_STRING,
+						'flags'     => FILTER_REQUIRE_ARRAY
+				),
+				'_pronamic_pay_gf_delay_post_creation'                => FILTER_VALIDATE_BOOLEAN,
+				'_pronamic_pay_gf_delay_campaignmonitor_subscription' => FILTER_VALIDATE_BOOLEAN,
+				'_pronamic_pay_gf_delay_mailchimp_subscription'       => FILTER_VALIDATE_BOOLEAN,
+				'_pronamic_pay_gf_delay_user_registration'            => FILTER_VALIDATE_BOOLEAN,
+				'_pronamic_pay_gf_fields'                             => array(
+						'filter'    => FILTER_SANITIZE_STRING,
+						'flags'     => FILTER_REQUIRE_ARRAY
+				),
+				'_pronamic_pay_gf_links' => array(
+						'filter'    => FILTER_SANITIZE_STRING,
+						'flags'     => FILTER_REQUIRE_ARRAY
+				),
+				'_pronamic_pay_gf_user_role_field_id'                 => FILTER_SANITIZE_STRING
+		);
+	
+		$data = filter_input_array( INPUT_POST, $definition );
+	
+		// Update post meta data
+		pronamic_pay_update_post_meta_data( $post_id, $data );
 	}
 }
