13ac7d78373f377ba80aa936cfe9e7ab2b2e27b7
Remco Tolsma <info@remcotolsma.nl>
2014-07-16 16:31:15 +0200

    Also allow null in the create function, will also return null then.

--- classes/Pronamic/Pay/GatewayFactory.php
+++ classes/Pronamic/Pay/GatewayFactory.php
@@ -7,16 +7,18 @@ class Pronamic_Pay_GatewayFactory {
 		self::$gateways[ $config_class ] = $gateway_class;
 	}
 
-	public static function create( Pronamic_WP_Pay_GatewayConfig $config ) {
+	public static function create( Pronamic_WP_Pay_GatewayConfig $config = null ) {
 		$gateway = null;
 
-		$config_class = get_class( $config );
+		if ( isset( $config ) ) {
+			$config_class = get_class( $config );
 
-		if ( isset( self::$gateways[ $config_class ] ) ) {
-			$gateway_class = self::$gateways[ $config_class ];
+			if ( isset( self::$gateways[ $config_class ] ) ) {
+				$gateway_class = self::$gateways[ $config_class ];
 
-			if ( class_exists( $gateway_class ) ) {
-				$gateway = new $gateway_class( $config );
+				if ( class_exists( $gateway_class ) ) {
+					$gateway = new $gateway_class( $config );
+				}
 			}
 		}
 
