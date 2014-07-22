2f397dacd5e291f7aa91ff3a2aeb633587b48861
Remco Tolsma <info@remcotolsma.nl>
2014-07-22 13:35:44 +0200

    Enabled Buckaroo listener.

--- classes/Pronamic/WP/Pay/Plugin.php
+++ classes/Pronamic/WP/Pay/Plugin.php
@@ -222,6 +222,7 @@ class Pronamic_WP_Pay_Plugin {
 		Pronamic_Gateways_Icepay_Listener::listen();
 		Pronamic_Gateways_Mollie_Listener::listen();
 		Pronamic_Pay_Gateways_Ogone_Listener::listen();
+		Pronamic_WP_Pay_Buckaroo_Listener::listen();
 	}
 
 	/**
