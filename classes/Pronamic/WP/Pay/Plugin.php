98444dd5ad33fc35571436959fc30d278563f8cc
Remco Tolsma <info@remcotolsma.nl>
2014-10-06 12:34:49 +0200

    Use the new Jigoshop class from the Jigoshop library (https://github.com/wp-pay-extensions/jigoshop).

--- classes/Pronamic/WP/Pay/Plugin.php
+++ classes/Pronamic/WP/Pay/Plugin.php
@@ -48,7 +48,7 @@ class Pronamic_WP_Pay_Plugin {
 			Pronamic_WP_Pay_Extensions_WooCommerce_Extension::bootstrap();
 			Pronamic_GravityForms_IDeal_AddOn::bootstrap();
 			Pronamic_Shopp_IDeal_AddOn::bootstrap();
-			Pronamic_Jigoshop_IDeal_AddOn::bootstrap();
+			Pronamic_WP_Pay_Extensions_Jigoshop_Extension::bootstrap();
 			Pronamic_WPeCommerce_IDeal_AddOn::bootstrap();
 			Pronamic_ClassiPress_IDeal_AddOn::bootstrap();
 			Pronamic_EventEspresso_IDeal_AddOn::bootstrap();
