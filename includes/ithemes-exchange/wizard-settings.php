73dff5c62216a2ba36017ae0f3e598c5b17659a4
Remco Tolsma <info@remcotolsma.nl>
2015-02-27 16:22:35 +0100

    Using correct class name.

--- includes/ithemes-exchange/wizard-settings.php
+++ includes/ithemes-exchange/wizard-settings.php
@@ -1,14 +1,14 @@
-<div class="field <?php echo Pronamic_IThemesExchange_IDeal_AddOn::$slug; ?>-wizard">
+<div class="field <?php echo Pronamic_WP_Pay_Extensions_IThemesExchange_Extension::$slug; ?>-wizard">
 	<h3><?php _e( 'iDEAL', 'pronamic_ideal' ); ?></h3>
 
-	<?php settings_fields( Pronamic_IThemesExchange_IDeal_AddOn::OPTION_GROUP ); ?>
+	<?php settings_fields( Pronamic_WP_Pay_Extensions_IThemesExchange_Extension::OPTION_GROUP ); ?>
 
-	<?php do_settings_sections( Pronamic_IThemesExchange_IDeal_AddOn::OPTION_GROUP ); ?>
+	<?php do_settings_sections( Pronamic_WP_Pay_Extensions_IThemesExchange_Extension::OPTION_GROUP ); ?>
 
 	<input
-		class="enable-<?php echo Pronamic_IThemesExchange_IDeal_AddOn::$slug; ?>"
+		class="enable-<?php echo Pronamic_WP_Pay_Extensions_IThemesExchange_Extension::$slug; ?>"
 		name="it-exchange-transaction-methods[]"
-		value="<?php echo Pronamic_IThemesExchange_IDeal_AddOn::$slug; ?>"
+		value="<?php echo Pronamic_WP_Pay_Extensions_IThemesExchange_Extension::$slug; ?>"
 		type="hidden"
 	/>
 </div>
