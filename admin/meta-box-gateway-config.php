eaaa557b1a0f8a9675f91c114f970d076f676b11
Remco Tolsma <info@remcotolsma.nl>
2015-03-08 20:32:25 +0100

    Move purchase key to iDEAL.

--- admin/meta-box-gateway-config.php
+++ admin/meta-box-gateway-config.php
@@ -70,6 +70,22 @@ $sections = array(
 				'description' => sprintf( __( 'You receive the sub ID from your iDEAL provider, the default is: %s.', 'pronamic_ideal' ), 0 ),
 				'methods'     => array( 'ideal_basic', 'ideal_advanced', 'ideal_advanced_v3' ),
 			),
+			array(
+				'meta_key'    => '_pronamic_gateway_ideal_purchase_id',
+				'title'       => __( 'Purchase ID', 'pronamic_ideal' ),
+				'type'        => 'text',
+				'classes'     => array( 'regular-text', 'code' ),
+				'description' => sprintf(
+					'%s<br />%s<br />%s',
+					sprintf(
+						__( 'This controls the iDEAL %s parameter.', 'pronamic_ideal' ),
+						sprintf( '<code>%s</code>', 'purchaseID' )
+					),
+					sprintf( __( 'Default: <code>%s</code>.', 'pronamic_ideal' ), '{payment_id}' ),
+					sprintf( __( 'Tags: %s', 'pronamic_ideal' ), sprintf( '<code>%s</code> <code>%s</code>', '{order_id}', '{payment_id}' ) )
+				),
+				'methods'     => array( 'ideal_basic', 'ideal_advanced', 'ideal_advanced_v3' ),
+			),
 		),
 	),
 	array(
@@ -82,22 +98,6 @@ $sections = array(
 				'type'        => 'text',
 				'classes'     => array( 'regular-text', 'code' ),
 				'description' => __( 'You configure the hash key (also known as: key or secret key) in the iDEAL dashboard of your iDEAL provider.', 'pronamic_ideal' ),
-				'methods'     => array( 'ideal_basic' ),
-			),
-			array(
-				'meta_key'    => '_pronamic_gateway_ideal_purchase_id',
-				'title'       => __( 'Purchase ID', 'pronamic_ideal' ),
-				'type'        => 'text',
-				'classes'     => array( 'regular-text', 'code' ),
-				'description' => sprintf(
-					'%s<br />%s<br />%s',
-					sprintf(
-						__( 'This controls the iDEAL Basic %s parameter.', 'pronamic_ideal' ),
-						sprintf( '<code>%s</code>', 'purchaseID' )
-					),
-					sprintf( __( 'Default: <code>%s</code>.', 'pronamic_ideal' ), '{payment_id}' ),
-					sprintf( __( 'Tags: %s', 'pronamic_ideal' ), sprintf( '<code>%s</code> <code>%s</code>', '{order_id}', '{payment_id}' ) )
-				),
 				'methods'     => array( 'ideal_basic' ),
 			),
 			array(
