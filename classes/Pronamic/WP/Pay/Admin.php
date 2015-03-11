37214fd8ff5d15211dfcadb85f10f45cc7e603e2
Remco Tolsma <info@remcotolsma.nl>
2015-03-11 15:11:45 +0100

    Updated copyright year to 2015 and @version and @since.

--- classes/Pronamic/WP/Pay/Admin.php
+++ classes/Pronamic/WP/Pay/Admin.php
@@ -3,10 +3,11 @@
 /**
  * Title: WordPress iDEAL admin
  * Description:
- * Copyright: Copyright (c) 2005 - 2011
+ * Copyright: Copyright (c) 2005 - 2015
  * Company: Pronamic
  * @author Remco Tolsma
- * @version 1.0
+ * @version 1.0.0
+ * @since 1.0.0
  */
 class Pronamic_WP_Pay_Admin {
 	/**
