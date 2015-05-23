e77a11906729372bfd8d427e641db906411a1543
Remco Tolsma <info@remcotolsma.nl>
2015-05-23 12:57:18 +0200

    Load JobRoller and eShop classes from the WordPress pay libraries.

--- classes/Pronamic/WP/Pay/Plugin.php
+++ classes/Pronamic/WP/Pay/Plugin.php
@@ -47,7 +47,7 @@ class Pronamic_WP_Pay_Plugin {
 		Pronamic_WP_Pay_Extensions_AppThemes_Extension::bootstrap();
 		Pronamic_WP_Pay_Extensions_S2Member_Extension::bootstrap();
 		Pronamic_WPMUDEV_Membership_IDeal_AddOn::bootstrap();
-		// Pronamic_EShop_IDeal_AddOn::bootstrap();
+		// Pronamic_WP_Pay_Extensions_EShop_Extension::bootstrap();
 		Pronamic_WP_Pay_Extensions_EDD_Extension::bootstrap();
 		Pronamic_WP_Pay_Extensions_IThemesExchange_Extension::bootstrap();
 
