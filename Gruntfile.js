76ebd2b271de52953fb593cfa1097f6acef36ef4
Remco Tolsma <info@remcotolsma.nl>
2015-05-26 15:58:15 +0200

    Ignore 'src' folder in deploy.

--- Gruntfile.js
+++ Gruntfile.js
@@ -172,6 +172,7 @@ module.exports = function( grunt ) {
 					'!deploy/**',
 					'!documentation/**',
 					'!node_modules/**',
+					'!src/**',
 					'!tests/**',
 					'!vendor/*/*/test/**',
 					'!vendor/*/*/tests/**',
