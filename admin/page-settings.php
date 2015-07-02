7196aac49aebf1afa803333cb0fa356d24ea28d1
Remco Tolsma <info@remcotolsma.nl>
2015-07-02 15:05:44 +0200

    Escape.

--- admin/page-settings.php
+++ admin/page-settings.php
@@ -1,5 +1,5 @@
 <div class="wrap">
-	<h2><?php echo get_admin_page_title(); ?></h2>
+	<h2><?php echo esc_html( get_admin_page_title() ); ?></h2>
 
 	<form action="options.php" method="post">
 		<?php settings_fields( 'pronamic_pay' ); ?>
