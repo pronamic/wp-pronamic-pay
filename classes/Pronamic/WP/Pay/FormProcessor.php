46c2e72a0be8cbe1ac55657b239a7d2918cc3171
Remco Tolsma <info@remcotolsma.nl>
2015-08-13 13:36:31 +0200

    Make sure to create an WordPress user for each payment.

--- classes/Pronamic/WP/Pay/FormProcessor.php
+++ classes/Pronamic/WP/Pay/FormProcessor.php
@@ -53,6 +53,37 @@ class Pronamic_WP_Pay_FormProcessor {
 						if ( is_wp_error( $error ) ) {
 							Pronamic_WP_Pay_Plugin::render_errors( $error );
 						} else {
+							// @see https://github.com/WordImpress/Give/blob/1.1/includes/payments/functions.php#L172-L178
+							// @see https://github.com/woothemes/woocommerce/blob/2.4.3/includes/wc-user-functions.php#L36-L118
+							$first_name = filter_input( INPUT_POST, 'pronamic_pay_first_name', FILTER_SANITIZE_STRING );
+							$last_name  = filter_input( INPUT_POST, 'pronamic_pay_last_name', FILTER_SANITIZE_STRING );
+							$email      = filter_input( INPUT_POST, 'pronamic_pay_email', FILTER_VALIDATE_EMAIL );
+
+							$user = get_user_by( 'email', $email );
+
+							if ( ! $user ) {
+								// Make a random string for password
+								$password = wp_generate_password( 10 );
+
+								// Make a user with the username as the email
+								$user_id = wp_insert_user( array(
+									'user_login' => $email,
+									'user_pass'  => $password,
+									'user_email' => $email,
+									'role'       => 'payer',
+									'first_name' => $first_name,
+									'last_name'  => $last_name,
+								) );
+
+								// User
+								$user = new WP_User( $user_id );
+							}
+
+							wp_update_post( array(
+								'ID' => $payment->post->ID,
+								'post_author' => $user->ID,
+							) );
+
 							$gateway->redirect( $payment );
 						}
 
