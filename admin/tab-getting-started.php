a995eeceb79b5227368204226e3218e8bcec084e
Remco Tolsma <info@remcotolsma.nl>
2015-08-27 14:42:35 +0200

    Working on getting started an new tabs.

--- admin/tab-getting-started.php
+++ admin/tab-getting-started.php
@@ -11,12 +11,51 @@ if ( ! defined( 'WPINC' ) ) {
 ?>
 <div class="feature-section two-col">
 	<div class="col">
-		<h3>Stap 1 - Gateway configuratie aanmaken</h3>
+		<h3><?php esc_html_e( 'Step 1 - Account Payment Provider', 'pronamic_ideal' ); ?></h3>
 		
 		<p>
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer accumsan lacus sed ante rhoncus maximus. Aliquam pulvinar arcu et felis viverra mollis posuere.
-		</p>
+			<?php
 
+esc_html_e( 'A payment provider is responsible for the actual processing of payment transactions.
+We are not a payment provider, you will need to close an account with a payment provider or bank.
+We support most popular payment providers and banks. 
+The Pronamic iDEAL plugin makes the link between the selected payment provider and the e-commerce plugin.'
+, 'pronamic_ideal' );
+
+			?>
+		</p>
+	</div>
+
+	<div class="col">
+		<img src="//s.w.org/images/core/4.2/press-this.jpg">
+	</div>
+</div>
+
+<div class="feature-section two-col">
+	<div class="col">
+		<img src="//s.w.org/images/core/4.2/press-this.jpg">
+	</div>
+
+	<div class="col">
+		<h3><?php esc_html_e( 'Step 2 - Add Gateway Configuration', 'pronamic_ideal' ); ?></h3>
+		
+		<p>
+			<?php
+
+esc_html_e( 'Create a new configuration via iDEAL » Configurations » Add new.
+The settings depend on the chosen payment provider.
+Follow the instructions of the most popular payment providers to use the correct settings.'
+, 'pronamic_ideal' );
+
+			?>
+		</p>
+	</div>
+</div>
+
+<div class="feature-section two-col">
+	<div class="col">
+		<h3><?php esc_html_e( 'Step 3 - Test Gateway Configuration', 'pronamic_ideal' ); ?></h3>
+		
 		<p>
 			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer accumsan lacus sed ante rhoncus maximus. Aliquam pulvinar arcu et felis viverra mollis posuere.
 		</p>
@@ -33,12 +72,8 @@ if ( ! defined( 'WPINC' ) ) {
 	</div>
 
 	<div class="col">
-		<h3>Stap 2 - Gateway configuratie testen</h3>
+		<h3><?php esc_html_e( 'Step 4 - Configure E-commerce Plugin', 'pronamic_ideal' ); ?></h3>
 		
-		<p>
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer accumsan lacus sed ante rhoncus maximus. Aliquam pulvinar arcu et felis viverra mollis posuere.
-		</p>
-
 		<p>
 			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer accumsan lacus sed ante rhoncus maximus. Aliquam pulvinar arcu et felis viverra mollis posuere.
 		</p>
--- admin/tab-new.php
+++ admin/tab-new.php
@@ -10,11 +10,7 @@ if ( ! defined( 'WPINC' ) ) {
 ?>
 <div class="feature-section two-col">
 	<div class="col">
-		<h3>Lorem Ipsum</h3>
-
-		<p>
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et facilisis libero, nec tempor ligula. Aenean vitae elementum orci. Maecenas magna felis nullam.
-		</p>
+		<h3><?php esc_html_e( 'Simple Payment Forms', 'pronamic_ideal' ); ?></h3>
 
 		<p>
 			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et facilisis libero, nec tempor ligula. Aenean vitae elementum orci. Maecenas magna felis nullam.
@@ -23,5 +19,75 @@ if ( ! defined( 'WPINC' ) ) {
 
 	<div class="col">
 		<img src="//s.w.org/images/core/4.2/press-this.jpg">
+	</div>
+</div>
+
+<div class="feature-section two-col">
+	<div class="col">
+		<img src="//s.w.org/images/core/4.2/press-this.jpg">
+	</div>
+
+	<div class="col">
+		<h3><?php esc_html_e( 'Payments Reports', 'pronamic_ideal' ); ?></h3>
+
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et facilisis libero, nec tempor ligula. Aenean vitae elementum orci. Maecenas magna felis nullam.
+		</p>
+	</div>
+</div>
+
+<div class="feature-section two-col">
+	<div class="col">
+		<h3><?php esc_html_e( 'Badge Pending Payments', 'pronamic_ideal' ); ?></h3>
+
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et facilisis libero, nec tempor ligula. Aenean vitae elementum orci. Maecenas magna felis nullam.
+		</p>
+	</div>
+
+	<div class="col">
+		<img src="//s.w.org/images/core/4.2/press-this.jpg">
+	</div>
+</div>
+
+<div class="feature-section two-col">
+	<div class="col">
+		<img src="//s.w.org/images/core/4.2/press-this.jpg">
+	</div>
+
+	<div class="col">
+		<h3><?php esc_html_e( 'Payment Status Filter', 'pronamic_ideal' ); ?></h3>
+
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et facilisis libero, nec tempor ligula. Aenean vitae elementum orci. Maecenas magna felis nullam.
+		</p>
+	</div>
+</div>
+
+<div class="feature-section two-col">
+	<div class="col">
+		<h3><?php esc_html_e( 'Edit Payment Status', 'pronamic_ideal' ); ?></h3>
+
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et facilisis libero, nec tempor ligula. Aenean vitae elementum orci. Maecenas magna felis nullam.
+		</p>
+	</div>
+
+	<div class="col">
+		<img src="//s.w.org/images/core/4.2/press-this.jpg">
+	</div>
+</div>
+
+<div class="feature-section two-col">
+	<div class="col">
+		<img src="//s.w.org/images/core/4.2/press-this.jpg">
+	</div>
+
+	<div class="col">
+		<h3><?php esc_html_e( 'Tour', 'pronamic_ideal' ); ?></h3>
+
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et facilisis libero, nec tempor ligula. Aenean vitae elementum orci. Maecenas magna felis nullam.
+		</p>
 	</div>
 </div>
