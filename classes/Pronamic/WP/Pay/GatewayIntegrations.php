ea8034037e39f31e1d4d75f9a246bba707cb7f3c
Remco Tolsma <info@remcotolsma.nl>
2016-03-21 14:26:27 +0100

    Added extra new line for the WordPress Coding Standards.

--- classes/Pronamic/WP/Pay/GatewayIntegrations.php
+++ classes/Pronamic/WP/Pay/GatewayIntegrations.php
@@ -5,6 +5,7 @@
  * Description:
  * Copyright: Copyright (c) 2005 - 2016
  * Company: Pronamic
+ *
  * @author Remco Tolsma
  * @version 3.8.0
  * @since 3.8.0
--- classes/Pronamic/WP/Pay/Settings.php
+++ classes/Pronamic/WP/Pay/Settings.php
@@ -5,6 +5,7 @@
  * Description:
  * Copyright: Copyright (c) 2005 - 2016
  * Company: Pronamic
+ *
  * @author Remco Tolsma
  * @version 1.0.0
  */
