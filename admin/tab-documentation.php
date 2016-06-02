2d50ade0360fa55fc360872b462758bbbe9e8aee
Reüel van der Steege <reuel@pronamic.nl>
2016-06-02 15:38:11 +0200

    Use alternate table rows on tab 'Documentation'

--- admin/tab-documentation.php
+++ admin/tab-documentation.php
@@ -614,61 +614,65 @@ $providers = array(
 
 		<?php foreach ( $providers as $provider ) : ?>
 
-			<tr>
-				<td colspan="4">
-					<strong><?php echo esc_html( $provider['name'] ); ?></strong>
-					<small><a href="<?php echo esc_attr( $provider['url'] ); ?>"><?php echo esc_html( $provider['url'] ); ?></a></small>
-				</td>
-			</tr>
+			<?php if ( isset( $provider['resources'] ) && ! empty( $provider['resources'] ) ) : ?>
 
-			<?php foreach ( $provider['resources'] as $resource ) : ?>
-
-				<?php
-
-				$href = null;
-
-				if ( isset( $resource['path'] ) ) {
-					$href = plugins_url( $resource['path'], Pronamic_WP_Pay_Plugin::$file );
-				}
-
-				if ( isset( $resource['url'] ) ) {
-					$href = $resource['url'];
-				}
-
-				$classes = array();
-
-				if ( isset( $resource['deprecated'] ) ) {
-					$classes[] = 'deprecated';
-				}
-
-				?>
-				<tr class="<?php echo esc_attr( implode( ' ', $classes ) ); ?>">
-					<td>
-						<a href="<?php echo esc_attr( $href ); ?>">
-							<?php echo esc_html( $resource['name'] ); ?>
-						</a>
-					</td>
-					<td>
-						<?php
-
-						if ( isset( $resource['date'] ) ) {
-							echo esc_html( $resource['date']->format( 'd-m-Y' ) );
-						}
-
-						?>
-					</td>
-					<td>
-						<?php
-
-						if ( isset( $resource['version'] ) ) {
-							echo esc_html( $resource['version'] );
-						}
-
-						?>
+				<tr class="alternate">
+					<td colspan="4">
+						<strong><?php echo esc_html( $provider['name'] ); ?></strong>
+						<small><a href="<?php echo esc_attr( $provider['url'] ); ?>"><?php echo esc_html( $provider['url'] ); ?></a></small>
 					</td>
 				</tr>
 
-			<?php endforeach; ?>
+				<?php foreach ( $provider['resources'] as $resource ) : ?>
+
+					<?php
+
+					$href = null;
+
+					if ( isset( $resource['path'] ) ) {
+						$href = plugins_url( $resource['path'], Pronamic_WP_Pay_Plugin::$file );
+					}
+
+					if ( isset( $resource['url'] ) ) {
+						$href = $resource['url'];
+					}
+
+					$classes = array();
+
+					if ( isset( $resource['deprecated'] ) ) {
+						$classes[] = 'deprecated';
+					}
+
+					?>
+					<tr class="<?php echo esc_attr( implode( ' ', $classes ) ); ?>">
+						<td>
+							<a href="<?php echo esc_attr( $href ); ?>">
+								<?php echo esc_html( $resource['name'] ); ?>
+							</a>
+						</td>
+						<td>
+							<?php
+
+							if ( isset( $resource['date'] ) ) {
+								echo esc_html( $resource['date']->format( 'd-m-Y' ) );
+							}
+
+							?>
+						</td>
+						<td>
+							<?php
+
+							if ( isset( $resource['version'] ) ) {
+								echo esc_html( $resource['version'] );
+							}
+
+							?>
+						</td>
+					</tr>
+
+				<?php endforeach; ?>
+
+			<?php endif; ?>
 
 		<?php endforeach; ?>
 
