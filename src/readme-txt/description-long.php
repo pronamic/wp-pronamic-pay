3db076fab287c1366afff8d8e7118664e9304943
Reüel van der Steege <reuel@pronamic.nl>
2016-08-11 16:21:45 +0200

    Update gateways, providers and long description.

--- src/readme-txt/description-long.php
+++ src/readme-txt/description-long.php
@@ -1,10 +1,19 @@
-*	Easy installation and configuration
-*	Automatic updates
+Pronamic iDEAL is the best plugin available to accept payments through iDEAL in WordPress. iDEAL is the most popular payment method in the Netherlands, so if you want to start with e-commerce in the Dutch market: you’ll need iDEAL. The plugin supports the Bancontact payment method too, which is commonly used in Belgium. With over 100,000 downloads, the plugin has proven itself as a reliable WordPress solution to use for your payments.
 
-= Extensions =
+= Key benefits =
+*   Quickly add and edit payment provider configurations
+*   Easily execute required iDEAL tests with custom amounts
+*   Automatic status updates in WordPress of webshop orders
+*   Continually updated to support the latest e-commerce plugins
+*   Built-in generation of required security certificates
+*   Works with all popular WordPress e-commerce plugins
+*   Easily switch between different payment providers
+*   Reliable payment solution, with over 100,000 downloads
+
+= Support WordPress e-commerce plugins =
 
 <?php include 'extensions.php'; ?>
 
-= Gateways =
+= Support payment providers =
 
 <?php include 'gateways.php'; ?>
