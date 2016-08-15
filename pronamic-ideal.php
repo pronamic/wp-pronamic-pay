301932508a1ac99bc074791d5fdf256d8c1e929d
Reüel van der Steege <reuel@pronamic.nl>
2016-08-15 16:23:59 +0200

    Update mimimum required WordPress version to 4.3 (because of `wp_roles()` usage)

--- pronamic-ideal.php
+++ pronamic-ideal.php
@@ -5,7 +5,7 @@ Plugin URI: http://www.pronamic.eu/plugins/pronamic-ideal/
 Description: The Pronamic iDEAL plugin allows you to easily add the iDEAL payment method to your WordPress website.
 
 Version: 4.3.0
-Requires at least: 3.6
+Requires at least: 4.3
 
 Author: Pronamic
 Author URI: http://www.pronamic.eu/
