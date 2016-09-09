c036e6f46f9c64bfa715cd908ba2eb87a64179e3
I <info@remcotolsma.nl>
2016-09-09 13:39:38 +0200

    Added notice for users with PHP < 5.3, still work in progress.

---
+++ admin/notice-update-php.php
@@ -0,0 +1,22 @@
+<div class="error">
+	<p>
+		<strong><?php esc_html_e( 'Pronamic iDEAL', 'pronamic_ideal' ); ?></strong> —
+		<?php
+
+		echo wp_kses(
+			sprintf(
+				__( 'Unfortunately the Pronamic iDEAL plugin will no longer work correctly in PHP versions older than 5.3. Read more information about <a href="%1$s" target="%2$s">how you can update</a>.', 'pronamic_ideal' ),
+				esc_attr( 'http://www.wpupdatephp.com/update/' ),
+				esc_attr( '_blank' )
+			),
+			array(
+				'a' => array(
+					'href'   => true,
+					'target' => true,
+				),
+			)
+		);
+
+		?>
+	</p>
+</div>
