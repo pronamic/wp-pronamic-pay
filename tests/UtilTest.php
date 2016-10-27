2aa21a69d33835fe4dbae627e05cba215993c280
Remco Tolsma <info@remcotolsma.nl>
2016-10-27 16:45:39 +0200

    Improved format price function and added unit tests.

--- tests/UtilTest.php
+++ tests/UtilTest.php
@@ -15,6 +15,29 @@ class Pronamic_WP_UtilTest extends PHPUnit_Framework_TestCase {
 
 		$result = Pronamic_WP_Util::to_string_boolean( $boolean );
 
-		 $this->assertEquals( 'false', $result );
+		$this->assertEquals( 'false', $result );
+	}
+
+	/**
+	 * Test format price.
+	 *
+	 * @dataProvider format_price_matrix_provider
+	 */
+	public function test_format_price( $amount, $currency, $expected ) {
+		$formatted = Pronamic_WP_Util::format_price( $amount );
+
+		$this->assertEquals( $expected, $formatted );
+	}
+
+	public function format_price_matrix_provider() {
+		return array(
+			array( '', null, null ),
+			array( false, null, null ),
+			array( array(), null, null ),
+			array( new stdClass(), null, null ),
+			array( 0, null, '€ 0.00' ),
+			array( 123, null, '€ 123.00' ),
+			array( 123456, null, '€ 123,456.00' ),
+		);
 	}
 }
