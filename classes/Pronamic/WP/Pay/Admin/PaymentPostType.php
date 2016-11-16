4793fc1ea32046bf9e1effce384056543f436f82
Remco Tolsma <info@remcotolsma.nl>
2016-11-16 14:00:35 +0100

    Added custom post updated messages for payment post type.

--- classes/Pronamic/WP/Pay/Admin/PaymentPostType.php
+++ classes/Pronamic/WP/Pay/Admin/PaymentPostType.php
@@ -46,6 +46,8 @@ class Pronamic_WP_Pay_Admin_PaymentPostType {
 
 		add_filter( 'default_hidden_columns', array( $this, 'default_hidden_columns' ) );
 
+		add_filter( 'post_updated_messages', array( $this, 'post_updated_messages' ) );
+
 		// Transition Post Status
 		add_action( 'transition_post_status', array( $this, 'transition_post_status' ), 10, 3 );
 
@@ -470,6 +472,48 @@ class Pronamic_WP_Pay_Admin_PaymentPostType {
 			do_action( 'pronamic_payment_status_update_' . $payment->source, $payment, $can_redirect );
 			do_action( 'pronamic_payment_status_update', $payment, $can_redirect );
 		}
+	}
+
+	/**
+	 * Post updated messages.
+	 *
+	 * @see https://codex.wordpress.org/Function_Reference/register_post_type
+	 * @see https://github.com/WordPress/WordPress/blob/4.4.2/wp-admin/edit-form-advanced.php#L134-L173
+	 * @see https://github.com/woothemes/woocommerce/blob/2.5.5/includes/admin/class-wc-admin-post-types.php#L111-L168
+	 * @param string $message
+	 * @return string
+	 */
+	public function post_updated_messages( $messages ) {
+		global $post;
+
+		// @see https://translate.wordpress.org/projects/wp/4.4.x/admin/nl/default?filters[status]=either&filters[original_id]=2352797&filters[translation_id]=37948900
+		$scheduled_date = date_i18n( __( 'M j, Y @ H:i', 'pronamic_ideal' ), strtotime( $post->post_date ) );
+
+		$messages[ self::POST_TYPE ] = array(
+			 0 => '', // Unused. Messages start at index 1.
+			 1 => __( 'Payment updated.', 'pronamic_ideal' ),
+			// @see https://translate.wordpress.org/projects/wp/4.4.x/admin/nl/default?filters[status]=either&filters[original_id]=2352799&filters[translation_id]=37947229
+			 2 => $messages['post'][2],
+			// @see https://translate.wordpress.org/projects/wp/4.4.x/admin/nl/default?filters[status]=either&filters[original_id]=2352800&filters[translation_id]=37947870
+			 3 => $messages['post'][3],
+			// @see https://translate.wordpress.org/projects/wp/4.4.x/admin/nl/default?filters[status]=either&filters[original_id]=2352798&filters[translation_id]=37947230
+			 4 => __( 'Payment updated.', 'pronamic_ideal' ),
+			/* translators: %s: date and time of the revision */
+			// @see https://translate.wordpress.org/projects/wp/4.4.x/admin/nl/default?filters[status]=either&filters[original_id]=2352801&filters[translation_id]=37947231
+			 5 => isset( $_GET['revision'] ) ? sprintf( __( 'Payment restored to revision from %s.', 'pronamic_ideal' ), wp_post_revision_title( (int) $_GET['revision'], false ) ) : false,
+			// @see https://translate.wordpress.org/projects/wp/4.4.x/admin/nl/default?filters[status]=either&filters[original_id]=2352802&filters[translation_id]=37949178
+			 6 => __( 'Payment published.', 'pronamic_ideal' ),
+			// @see https://translate.wordpress.org/projects/wp/4.4.x/admin/nl/default?filters[status]=either&filters[original_id]=2352803&filters[translation_id]=37947232
+			 7 => __( 'Payment saved.', 'pronamic_ideal' ),
+			// @see https://translate.wordpress.org/projects/wp/4.4.x/admin/nl/default?filters[status]=either&filters[original_id]=2352804&filters[translation_id]=37949303
+			 8 => __( 'Payment submitted.', 'pronamic_ideal' ),
+			// @see https://translate.wordpress.org/projects/wp/4.4.x/admin/nl/default?filters[status]=either&filters[original_id]=2352805&filters[translation_id]=37949302
+			 9 => sprintf( __( 'Payment scheduled for: %s.', 'pronamic_ideal' ), '<strong>' . $scheduled_date . '</strong>' ),
+			// @https://translate.wordpress.org/projects/wp/4.4.x/admin/nl/default?filters[status]=either&filters[original_id]=2352806&filters[translation_id]=37949301
+			10 => __( 'Payment draft updated.', 'pronamic_ideal' ),
+		);
+
+		return $messages;
 	}
 
 	/**
