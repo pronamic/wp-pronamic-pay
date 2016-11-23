d66958f20e26f17f3ad7cca56f850d294dc4e97e
Remco Tolsma <info@remcotolsma.nl>
2016-11-23 15:52:13 +0100

    Auto deactivate when using PHP versions older than 5.3, als don’t allow activation on older PHP versions.

--- pronamic-ideal.php
+++ pronamic-ideal.php
@@ -19,6 +19,31 @@ GitHub URI: https://github.com/pronamic/wp-pronamic-ideal
 */
 
 /**
+ * Dependency-checking
+ */
+function pronamic_ideal_deactivate() {
+	deactivate_plugins( plugin_basename( __FILE__ ) );
+}
+
+function pronamic_ideal_block_activation() {
+	$message = sprintf(
+		__( 'Unfortunately the Pronamic iDEAL plugin will no longer work correctly in PHP versions older than 5.3. Read more information about <a href="%1$s" target="%2$s">how you can update</a>.', 'pronamic_ideal' ),
+		esc_attr__( 'http://www.wpupdatephp.com/update/', 'pronamic_ideal' ),
+		esc_attr( '_blank' )
+	);
+
+	wp_die( $message );
+}
+
+if ( version_compare( PHP_VERSION, '5.3', '<' ) ) {
+	register_activation_hook( __FILE__, 'pronamic_ideal_block_activation' );
+
+	add_action( 'admin_init', 'pronamic_ideal_deactivate' );
+
+	return;
+}
+
+/**
  * Autoload
  */
 if ( version_compare( PHP_VERSION, '5.3', '>=' ) ) {
