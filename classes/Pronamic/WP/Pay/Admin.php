c12a2628f4e731c58eb7a436b627543b9c867281
Reüel van der Steege <reuel@pronamic.nl>
2016-12-09 14:11:29 +0100

    WordPress Coding Standards.

--- classes/Pronamic/WP/Pay/Admin.php
+++ classes/Pronamic/WP/Pay/Admin.php
@@ -66,7 +66,7 @@ class Pronamic_WP_Pay_Admin {
 		// Gateway settings
 		$this->gateway_settings = new Pronamic_WP_Pay_Admin_GatewaySettings();
 
-		if ( ! wp_next_scheduled ( 'pronamic_pay_license_check' ) ) {
+		if ( ! wp_next_scheduled( 'pronamic_pay_license_check' ) ) {
 			wp_schedule_event( time(), 'daily', 'pronamic_pay_license_check' );
 		}
 	}
