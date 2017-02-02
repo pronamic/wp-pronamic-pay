1379bae1d7acac11e288e0230d35b3ca60b602b4
Remco Tolsma <info@remcotolsma.nl>
2017-02-02 21:49:02 +0100

    grunt deploy

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # [Pronamic iDEAL](https://www.pronamic.eu/plugins/pronamic-ideal/)
 
-[![Build Status](https://secure.travis-ci.org/pronamic/wp-pronamic-ideal.png?branch=develop)](http://travis-ci.org/pronamic/wp-pronamic-ideal)
+[![Build Status](https://travis-ci.org/pronamic/wp-pronamic-ideal.svg?branch=develop)](https://travis-ci.org/pronamic/wp-pronamic-ideal)
 [![Built with Grunt](https://cdn.gruntjs.com/builtwith.svg)](http://gruntjs.com/)
 
 The Pronamic iDEAL plugin allows you to easily add the iDEAL payment method to your WordPress website.
--- src/readme-md/README.php
+++ src/readme-md/README.php
@@ -5,7 +5,7 @@ header( 'Content-Type: text/plain' );
 ?>
 # [Pronamic iDEAL](https://www.pronamic.eu/plugins/pronamic-ideal/)
 
-[![Build Status](https://secure.travis-ci.org/pronamic/wp-pronamic-ideal.png?branch=develop)](http://travis-ci.org/pronamic/wp-pronamic-ideal)
+[![Build Status](https://travis-ci.org/pronamic/wp-pronamic-ideal.svg?branch=develop)](https://travis-ci.org/pronamic/wp-pronamic-ideal)
 [![Built with Grunt](https://cdn.gruntjs.com/builtwith.svg)](http://gruntjs.com/)
 
 <?php include __DIR__ . '/../general/description-short.php'; ?>
