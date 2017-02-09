8c23403458682a928d81a205ac78084d2fd10ac8
Remco Tolsma <info@remcotolsma.nl>
2017-02-09 12:40:45 +0100

    Getting ready for version 4.5.1.

--- includes/version.php
+++ includes/version.php
@@ -6,7 +6,7 @@
  */
 global $pronamic_pay_version;
 
-$pronamic_pay_version = '4.5.0';
+$pronamic_pay_version = '4.5.1';
 
 /**
  * Holds the Pronamic Pay DB revision, increments when changes are made to the Pronamic Pay DB schema.
@@ -15,4 +15,4 @@ $pronamic_pay_version = '4.5.0';
  */
 global $pronamic_pay_db_version;
 
-$pronamic_pay_db_version = 450;
+$pronamic_pay_db_version = 451;
