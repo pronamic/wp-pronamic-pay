31b78bcdd6838c6e6bfcb559601b3d8273254321
Reüel van der Steege <reuel@pronamic.nl>
2017-06-01 09:20:00 +0200

    Update readme 'Tested up to' to WordPress 4.8

--- src/readme-txt/readme.php
+++ src/readme-txt/readme.php
@@ -11,7 +11,7 @@ Contributors: pronamic, remcotolsma
 Tags: ideal, bank, payment, gravity forms, gravity, forms, form, payment, woocommerce, woothemes, shopp, rabobank, friesland bank, ing, mollie, omnikassa, wpsc, wpecommerce, commerce, e-commerce, cart, classipress, appthemes
 Donate link: https://www.pronamic.eu/donate/?for=wp-plugin-pronamic-ideal&source=wp-plugin-readme-txt
 Requires at least: 4.7
-Tested up to: 4.7.4
+Tested up to: 4.8
 Stable tag: <?php echo $pkg->version, "\r\n"; ?>
 
 <?php include __DIR__ . '/../general/description-short.php'; ?>
