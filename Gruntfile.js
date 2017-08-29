ca55275f2da1fc40cc7a04e6e52a531971a53384
Remco Tolsma <info@remcotolsma.nl>
2017-08-29 12:39:38 +0200

    Also ignore new package-lock.json files in deploy.

--- Gruntfile.js
+++ Gruntfile.js
@@ -277,6 +277,7 @@ module.exports = function( grunt ) {
 					'!composer.lock',
 					'!Gruntfile.js',
 					'!package.json',
+					'!package-lock.json',
 					'!phpunit.xml',
 					'!phpunit.xml.dist',
 					'!phpcs.ruleset.xml',
@@ -397,6 +398,7 @@ module.exports = function( grunt ) {
 					'deploy/latest/vendor/wp-pay*/*/.travis.yml',
 					'deploy/latest/vendor/wp-pay*/*/Gruntfile.js',
 					'deploy/latest/vendor/wp-pay*/*/package.json',
+					'deploy/latest/vendor/wp-pay*/*/package-lock.json',
 					'deploy/latest/vendor/wp-pay*/*/phpcs.ruleset.xml',
 					'deploy/latest/vendor/wp-pay*/*/phpmd.ruleset.xml',
 					'deploy/latest/vendor/wp-pay*/*/phpunit.xml.dist'
