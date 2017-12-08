1494f83c2d38c70c294f8b8697772d0d37cfa629
Remco Tolsma <info@remcotolsma.nl>
2017-12-08 15:12:52 +0100

    Renamed function from `pronamic_ideal_` to `pronamic_pay_`.

--- pronamic-ideal.php
+++ pronamic-ideal.php
@@ -21,14 +21,14 @@ GitHub URI: https://github.com/pronamic/wp-pronamic-ideal
 /**
  * Dependency-checking
  */
-function pronamic_ideal_deactivate() {
+function pronamic_pay_deactivate() {
 	deactivate_plugins( plugin_basename( __FILE__ ) );
 }
 
-function pronamic_ideal_block_activation() {
+function pronamic_pay_block_activation() {
 	$message = sprintf(
 		/* translators: 1: http://www.wpupdatephp.com/update/, 2: _blank */
-		__( 'Unfortunately the Pronamic iDEAL plugin will no longer work correctly in PHP versions older than 5.3. Read more information about <a href="%1$s" target="%2$s">how you can update</a>.', 'pronamic_ideal' ),
+		__( 'Unfortunately the Pronamic Pay plugin will no longer work correctly in PHP versions older than 5.3. Read more information about <a href="%1$s" target="%2$s">how you can update</a>.', 'pronamic_ideal' ),
 		esc_attr__( 'http://www.wpupdatephp.com/update/', 'pronamic_ideal' ),
 		esc_attr( '_blank' )
 	);
@@ -42,9 +42,9 @@ function pronamic_ideal_block_activation() {
 }
 
 if ( version_compare( PHP_VERSION, '5.3', '<' ) ) {
-	register_activation_hook( __FILE__, 'pronamic_ideal_block_activation' );
+	register_activation_hook( __FILE__, 'pronamic_pay_block_activation' );
 
-	add_action( 'admin_init', 'pronamic_ideal_deactivate' );
+	add_action( 'admin_init', 'pronamic_pay_deactivate' );
 
 	return;
 }
