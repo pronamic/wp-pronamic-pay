dec23f122677028d9ec04f5134433662b77822f3
Remco Tolsma <info@remcotolsma.nl>
2018-01-11 15:43:29 +0100

    Implemented early returns.

--- classes/Forms/FormProcessor.php
+++ classes/Forms/FormProcessor.php
@@ -58,68 +58,82 @@ class FormProcessor {
 
 		$pronamic_pay_errors = array();
 
-		if ( filter_has_var( INPUT_POST, 'pronamic_pay_nonce' ) ) {
-			$nonce = filter_input( INPUT_POST, 'pronamic_pay_nonce', FILTER_SANITIZE_STRING );
-
-			if ( wp_verify_nonce( $nonce, 'pronamic_pay' ) ) {
-				$id = filter_input( INPUT_POST, 'pronamic_pay_form_id', FILTER_VALIDATE_INT );
-
-				$config_id = get_post_meta( $id, '_pronamic_payment_form_config_id', true );
-
-				$valid = $this->validate();
-
-				if ( $valid ) {
-					$gateway = \Pronamic_WP_Pay_Plugin::get_gateway( $config_id );
-
-					if ( $gateway ) {
-						$data = new PaymentFormData();
-
-						$payment = \Pronamic_WP_Pay_Plugin::start( $config_id, $gateway, $data );
-
-						$error = $gateway->get_error();
-
-						if ( is_wp_error( $error ) ) {
-							\Pronamic_WP_Pay_Plugin::render_errors( $error );
-						} else {
-							// @see https://github.com/WordImpress/Give/blob/1.1/includes/payments/functions.php#L172-L178
-							// @see https://github.com/woothemes/woocommerce/blob/2.4.3/includes/wc-user-functions.php#L36-L118
-							$first_name = filter_input( INPUT_POST, 'pronamic_pay_first_name', FILTER_SANITIZE_STRING );
-							$last_name  = filter_input( INPUT_POST, 'pronamic_pay_last_name', FILTER_SANITIZE_STRING );
-							$email      = filter_input( INPUT_POST, 'pronamic_pay_email', FILTER_VALIDATE_EMAIL );
-
-							$user = get_user_by( 'email', $email );
-
-							if ( ! $user ) {
-								// Make a random string for password
-								$password = wp_generate_password( 10 );
-
-								// Make a user with the username as the email
-								$user_id = wp_insert_user( array(
-									'user_login' => $email,
-									'user_pass'  => $password,
-									'user_email' => $email,
-									'role'       => 'payer',
-									'first_name' => $first_name,
-									'last_name'  => $last_name,
-								) );
-
-								// User
-								$user = new \WP_User( $user_id );
-							}
-
-							wp_update_post( array(
-								'ID' => $payment->post->ID,
-								'post_author' => $user->ID,
-							) );
-
-							$gateway->redirect( $payment );
-						}
-
-						exit;
-					}
-				}
-			}
+		// Nonce
+		if ( ! filter_has_var( INPUT_POST, 'pronamic_pay_nonce' ) ) {
+			return;
 		}
+		
+		$nonce = filter_input( INPUT_POST, 'pronamic_pay_nonce', FILTER_SANITIZE_STRING );
+
+		if ( ! wp_verify_nonce( $nonce, 'pronamic_pay' ) ) {
+			return;
+		}
+
+		// Validate
+		$valid = $this->validate();
+
+		if ( ! $valid ) {
+			return;
+		}
+
+		// Gateway
+		$id = filter_input( INPUT_POST, 'pronamic_pay_form_id', FILTER_VALIDATE_INT );
+
+		$config_id = get_post_meta( $id, '_pronamic_payment_form_config_id', true );
+
+		$gateway = \Pronamic_WP_Pay_Plugin::get_gateway( $config_id );
+
+		if ( ! $gateway ) {
+			return;
+		}
+
+		// Data
+		$data = new PaymentFormData();
+
+		$payment = \Pronamic_WP_Pay_Plugin::start( $config_id, $gateway, $data );
+
+		$error = $gateway->get_error();
+
+		if ( is_wp_error( $error ) ) {
+			\Pronamic_WP_Pay_Plugin::render_errors( $error );
+
+			exit;
+		}
+
+		// @see https://github.com/WordImpress/Give/blob/1.1/includes/payments/functions.php#L172-L178
+		// @see https://github.com/woothemes/woocommerce/blob/2.4.3/includes/wc-user-functions.php#L36-L118
+		$first_name = filter_input( INPUT_POST, 'pronamic_pay_first_name', FILTER_SANITIZE_STRING );
+		$last_name  = filter_input( INPUT_POST, 'pronamic_pay_last_name', FILTER_SANITIZE_STRING );
+		$email      = filter_input( INPUT_POST, 'pronamic_pay_email', FILTER_VALIDATE_EMAIL );
+
+		$user = get_user_by( 'email', $email );
+
+		if ( ! $user ) {
+			// Make a random string for password
+			$password = wp_generate_password( 10 );
+
+			// Make a user with the username as the email
+			$user_id = wp_insert_user( array(
+				'user_login' => $email,
+				'user_pass'  => $password,
+				'user_email' => $email,
+				'role'       => 'payer',
+				'first_name' => $first_name,
+				'last_name'  => $last_name,
+			) );
+
+			// User
+			$user = new \WP_User( $user_id );
+		}
+
+		wp_update_post( array(
+			'ID'          => $payment->post->ID,
+			'post_author' => $user->ID,
+		) );
+
+		$gateway->redirect( $payment );
+
+		exit;
 	}
 
 	private function validate() {
