516a70c2e2a15ac69e7e60ad8274439d743d6041
Remco Tolsma <info@remcotolsma.nl>
2018-03-21 15:31:27 +0100

    Introduced a custom DateTime and DateTimeZone class with a `date_i18n` function, makes it easier to render a datetime in the local time of a WordPress installation.

---
+++ classes/DateTime.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Date time
+ *
+ * @author    Pronamic <info@pronamic.eu>
+ * @copyright 2005-2018 Pronamic
+ * @license   GPL-3.0-or-later
+ * @package   Pronamic\WordPress\Pay
+ * @see       https://github.com/woocommerce/woocommerce/blob/3.3.4/includes/class-wc-datetime.php
+ * @see       https://github.com/Rarst/wpdatetime/
+ */
+
+namespace Pronamic\WordPress\Pay;
+
+/**
+ * Date time
+ *
+ * @author Remco Tolsma
+ * @version 1.0
+ */
+class DateTime extends \DateTime {
+	/**
+	 * Date I18N.
+	 *
+	 * @see https://github.com/Rarst/wpdatetime/blob/0.3/src/WpDateTimeTrait.php#L79-L104
+	 * @see https://github.com/WordPress/WordPress/blob/4.9.4/wp-includes/functions.php#L72-L151
+	 *
+	 * @param string $format Format.
+	 *
+	 * @return string
+	 */
+	public function date_i18n( $format ) {
+		$date = clone $this;
+
+		$date->setTimezone( DateTimeZone::get_default() );
+
+		$result = date_i18n( $format, $date->getTimestamp() + $date->getOffset(), true );
+
+		return $result;
+	}
+}
