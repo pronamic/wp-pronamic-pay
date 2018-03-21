984b92ae819a0f3e77e54e1ba08779343a3913c8
Remco Tolsma <info@remcotolsma.nl>
2018-03-21 15:37:15 +0100

    Added some more setter and getter tests.

--- tests/Subscriptions/SubscriptionTest.php
+++ tests/Subscriptions/SubscriptionTest.php
@@ -29,6 +29,75 @@ class SubscriptionTest extends WP_UnitTestCase {
 	}
 
 	/**
+	 * Test set and get.
+	 *
+	 * @dataProvider get_and_set_provider
+	 */
+	public function test_set_and_get( $set_function, $get_function, $value ) {
+		$subscription = new Subscription();
+
+		$subscription->$set_function( $value );
+
+		$this->assertEquals( $value, $subscription->$get_function() );
+	}
+
+	public function get_and_set_provider() {
+		return array(
+			array( 'set_id',             'get_id',             uniqid()    ),
+			array( 'set_status',         'get_status',         'completed' ),
+			array( 'set_transaction_id', 'get_transaction_id', uniqid()    ),
+		);
+	}
+
+	/**
+	 * Test set.
+	 *
+	 * @dataProvider set_provider
+	 */
+	public function test_set( $set_function, $property, $value ) {
+		$subscription = new Subscription();
+
+		$subscription->$set_function( $value );
+
+		$this->assertEquals( $value, $subscription->$property );
+	}
+
+	public function set_provider() {
+		return array(
+			array( 'set_consumer_name', 'consumer_name', 'John Doe' ),
+			array( 'set_consumer_iban', 'consumer_iban', 'NL56 RABO 0108 6347 79' ),
+			array( 'set_consumer_bic',  'consumer_bic',  'RABONL2U' ),
+		);
+	}
+
+	/**
+	 * Test get.
+	 *
+	 * @dataProvider get_provider
+	 */
+	public function test_get( $property, $get_function, $value ) {
+		$subscription = new Subscription();
+
+		$subscription->$property = $value;
+
+		$this->assertEquals( $value, $subscription->$get_function() );
+	}
+
+	public function get_provider() {
+		return array(
+			array( 'key',                 'get_key',             uniqid() ),
+			array( 'source',              'get_source',          'woocommerce' ),
+			array( 'source_id',           'get_source_id',       '1234' ),
+			array( 'frequency',           'get_frequency',       'daily' ),
+			array( 'interval',            'get_interval',        '1' ),
+			array( 'interval_period',     'get_interval_period', 'Y' ),
+			array( 'description',         'get_description',     'Lorem ipsum dolor sit amet, consectetur.' ),
+			array( 'currency',            'get_currency',        'EUR' ),
+			array( 'amount',              'get_amount',          89.95 ),
+		);
+	}
+
+	/**
 	 * Test getting no subscription.
 	 *
 	 * @see https://github.com/easydigitaldownloads/easy-digital-downloads/blob/2.8.18/tests/tests-payment-class.php#L70-L79
@@ -37,31 +106,5 @@ class SubscriptionTest extends WP_UnitTestCase {
 		$subscription = new Subscription();
 
 		$this->assertNull( $subscription->get_id() );
-	}
-
-	/** 
-	 * Test setting and getting the subscription ID.
-	 */
-	public function test_set_and_get_id() {
-		$subscription = new Subscription();
-
-		$id = uniqid();
-
-		$subscription->set_id( $id );
-
-		$this->assertEquals( $id, $subscription->get_id() );
-	}
-
-	/** 
-	 * Test setting and getting the subscription status.
-	 */
-	public function test_set_and_get_status() {
-		$subscription = new Subscription();
-
-		$status = 'completed';
-
-		$subscription->set_status( $status );
-
-		$this->assertEquals( $status, $subscription->get_status() );
 	}
 }
