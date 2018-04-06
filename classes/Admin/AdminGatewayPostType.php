02fe7f1a8d4dbc4059d3d2f22a6ce6ca82736d2c
Remco Tolsma <info@remcotolsma.nl>
2018-04-06 13:47:11 +0200

    Fixed "The property plugin is declared private in Pronamic\WordPress\Pay\Admin\AdminModule and cannot be accessed from this context.".

--- classes/Admin/AdminGatewayPostType.php
+++ classes/Admin/AdminGatewayPostType.php
@@ -90,7 +90,7 @@ class AdminGatewayPostType {
 	public function custom_columns( $column, $post_id ) {
 		$id = get_post_meta( $post_id, '_pronamic_gateway_id', true );
 
-		$integrations = $this->admin->plugin->gateway_integrations;
+		$integrations = $this->plugin->gateway_integrations;
 
 		if ( isset( $integrations[ $id ] ) ) {
 			$integration = $integrations[ $id ];
