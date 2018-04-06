6165e430a267827a66e398d19361f6babaeaf8bc
Remco Tolsma <info@remcotolsma.nl>
2018-04-06 10:28:23 +0200

    Removed the old `Pronamic_Gateways_IDealAdvanced_Error`, is no longer included in the plugin.

--- views/error.php
+++ views/error.php
@@ -25,35 +25,6 @@ if ( is_wp_error( $error ) ) : ?>
 
 			<?php
 
-			if ( 'ideal_advanced_error' === $code ) {
-				$ideal_error = $error->get_error_data( $code );
-
-				if ( $ideal_error instanceof Pronamic_Gateways_IDealAdvanced_Error ) :
-
-					?>
-
-					<dl>
-						<dt><?php esc_html_e( 'Code', 'pronamic_ideal' ); ?></dt>
-						<dd><?php echo esc_html( $ideal_error->get_code() ); ?></dd>
-
-						<dt><?php esc_html_e( 'Message', 'pronamic_ideal' ); ?></dt>
-						<dd><?php echo esc_html( $ideal_error->get_message() ); ?></dd>
-
-						<dt><?php esc_html_e( 'Detail', 'pronamic_ideal' ); ?></dt>
-						<dd><?php echo esc_html( $ideal_error->get_detail() ); ?></dd>
-
-						<dt><?php esc_html_e( 'Suggested Action', 'pronamic_ideal' ); ?></dt>
-						<dd><?php echo esc_html( $ideal_error->get_suggested_action() ); ?></dd>
-
-						<dt><?php esc_html_e( 'Consumer Message', 'pronamic_ideal' ); ?></dt>
-						<dd><?php echo esc_html( $ideal_error->get_consumer_message() ); ?></dd>
-					</dl>
-
-					<?php
-
-				endif;
-			}
-
 			if ( 'ideal_advanced_v3_error' === $code ) {
 				$ideal_error = $error->get_error_data( $code );
 
