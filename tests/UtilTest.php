c6761125ec147db79a1a7b90061104a28f097b57
Remco Tolsma <info@remcotolsma.nl>
2018-05-15 11:08:34 +0200

    Removed tests of the removed `format_price` function.

--- tests/UtilTest.php
+++ tests/UtilTest.php
@@ -22,27 +22,4 @@ class UtilTest extends WP_UnitTestCase {
 
 		$this->assertEquals( 'false', $result );
 	}
-
-	/**
-	 * Test format price.
-	 *
-	 * @dataProvider format_price_matrix_provider
-	 */
-	public function test_format_price( $amount, $currency, $expected ) {
-		$formatted = Util::format_price( $amount );
-
-		$this->assertEquals( $expected, $formatted );
-	}
-
-	public function format_price_matrix_provider() {
-		return array(
-			array( '', null, null ),
-			array( false, null, null ),
-			array( array(), null, null ),
-			array( new stdClass(), null, null ),
-			array( 0, null, '€ 0,00' ),
-			array( 123, null, '€ 123,00' ),
-			array( 123456, null, '€ 123.456,00' ),
-		);
-	}
 }
