06f00d044e3b5e996dbc22cca1d8e9cf3111394f
Reüel van der Steege <reuel@pronamic.nl>
2018-05-24 11:32:26 +0200

    Do not use backticks for shell commands.

--- src/repositories.php
+++ src/repositories.php
@@ -69,14 +69,14 @@ foreach ( $organisations as $organisation => $repositories ) {
 		$git_dir = $repositories_dir . '/' . $organisation . '/' . $repository;
 
 		if ( ! is_dir( $git_dir ) ) {
-			`git clone $git_url $git_dir`;
+			echo shell_exec( 'git clone ' . $git_url . ' ' . $git_dir );
 		}
 
 		// Git flow
 		chdir( $git_dir );
 
 		if ( isset( $argv[1] ) && 'develop' === $argv[1] ) {
-			`git checkout develop`;
+			echo shell_exec( 'git checkout develop' );
 		}
 
 		if ( isset( $argv[1] ) && 'pull' === $argv[1] ) {
