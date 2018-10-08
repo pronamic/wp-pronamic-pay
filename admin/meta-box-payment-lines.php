7f48bbde83f9b5ed913989b5a6c22b50c4611d2a
Remco Tolsma <info@remcotolsma.nl>
2018-10-08 19:49:19 +0200

    Working on payment lines meta box.

--- admin/meta-box-payment-lines.php
+++ admin/meta-box-payment-lines.php
@@ -24,22 +24,101 @@ if ( empty( $lines ) ) : ?>
 	<table class="pronamic-pay-table widefat">
 		<thead>
 			<tr>
-				<th scope="col"><?php esc_html_e( 'Item ID', 'pronamic_ideal' ); ?></th>
+				<th scope="col"><?php esc_html_e( 'ID', 'pronamic_ideal' ); ?></th>
+				<th scope="col"><?php esc_html_e( 'SKU', 'pronamic_ideal' ); ?></th>
+				<th scope="col"><?php esc_html_e( 'Name', 'pronamic_ideal' ); ?></th>
 				<th scope="col"><?php esc_html_e( 'Description', 'pronamic_ideal' ); ?></th>
+				<th scope="col"><?php esc_html_e( 'Unit Price', 'pronamic_ideal' ); ?></th>
 				<th scope="col"><?php esc_html_e( 'Quantity', 'pronamic_ideal' ); ?></th>
-				<th scope="col"><?php esc_html_e( 'Pricy', 'pronamic_ideal' ); ?></th>
+				<th scope="col"><?php esc_html_e( 'Total Amount', 'pronamic_ideal' ); ?></th>
+				<th scope="col"><?php esc_html_e( 'Total Tax', 'pronamic_ideal' ); ?></th>
 			</tr>
 		</thead>
+
+		<tfoot>
+			<tr>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td>
+					<?php
+
+					$quantities = array_map( function( $line ) {
+						return $line->get_quantity();
+					}, $lines->get_array() );
+
+					echo esc_html( array_sum( $quantities ) );
+
+					?>
+				</td>
+				<td>
+					<?php
+
+					$values = array_map( function( $line ) {
+						if ( null !== $line->get_total_amount() ) {
+							return $line->get_total_amount()->get_amount();
+						}
+					}, $lines->get_array() );
+
+					echo esc_html( array_sum( $values ) );
+
+					?>
+				</td>
+				<td>
+					<?php
+
+					$values = array_map( function( $line ) {
+						if ( null !== $line->get_total_tax() ) {
+							return $line->get_total_tax()->get_amount();
+						}
+					}, $lines->get_array() );
+
+					echo esc_html( array_sum( $values ) );
+
+					?>
+				</td>
+			</tr>
+		</tfoot>
 
 		<tbody>
 
 			<?php foreach ( $lines as $line ) : ?>
 
 				<tr>
-					<td><?php echo esc_html( $item->get_id() ); ?></td>
-					<td><?php echo esc_html( $item->get_description() ); ?></td>
-					<td><?php echo esc_html( $item->get_quantity() ); ?></td>
-					<td><?php echo esc_html( $amount->format_i18n() ); ?></td>
+					<td><?php echo esc_html( $line->get_id() ); ?></td>
+					<td><?php echo esc_html( $line->get_sku() ); ?></td>
+					<td><?php echo esc_html( $line->get_name() ); ?></td>
+					<td><?php echo esc_html( $line->get_description() ); ?></td>
+					<td>
+						<?php
+
+						if ( null !== $line->get_unit_price() ) {
+							echo esc_html( $line->get_unit_price()->format_i18n() );
+						}
+
+						?>
+					</td>
+					<td><?php echo esc_html( $line->get_quantity() ); ?></td>
+					<td>
+						<?php
+
+						if ( null !== $line->get_total_amount() ) {
+							echo esc_html( $line->get_total_amount()->format_i18n() );
+						}
+
+						?>
+					</td>
+					<td>
+						<?php
+
+						if ( null !== $line->get_total_tax() ) {
+							echo esc_html( $line->get_total_tax()->format_i18n() );
+						}
+
+						?>
+					</td>
 				</tr>
 
 			<?php endforeach; ?>
