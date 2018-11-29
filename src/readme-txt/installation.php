1af8d598efe3fa12db623a0db89b1f02db5be2d4
Reüel van der Steege <reuel@pronamic.nl>
2018-11-29 16:45:56 +0100

    Replace last occurences of "Pronamic iDEAL" with renamed "Pronamic Pay".

--- src/readme-txt/installation.php
+++ src/readme-txt/installation.php
@@ -1,16 +1,16 @@
 = Requirements =
 
-The Pronamic Pay plugin extends WordPress extensions with the iDEAL payment method. To offer the iDEAL payment method to the vistors of your WordPress website you also require one of these extensions. Normally this is an e-commerce or forms plugin. 
+The Pronamic Pay plugin extends WordPress extensions with payment methods such as iDEAL, Bancontact, Sofort and credit cards. To offer the payment methods to the vistors of your WordPress website you also require one of these e-commerce or form builder extensions.
 
 = Automatic installation =
 
-Automatic installation is the easiest option as WordPress handles the file transfers itself and you don’t need to leave your web browser. To do an automatic install of Pronamic iDEAL, log in to your WordPress dashboard, navigate to the Plugins menu and click Add New.
+Automatic installation is the easiest option as WordPress handles the file transfers itself and you don’t need to leave your web browser. To do an automatic install of Pronamic Pay, log in to your WordPress dashboard, navigate to the Plugins menu and click Add New.
 
-In the search field type “Pronamic iDEAL” and click Search Plugins. Once you’ve found our iDEAL plugin you can view details about it such as the the point release, rating and description. Most importantly of course, you can install it by simply clicking “Install Now”.
+In the search field type “Pronamic Pay” and click Search Plugins. Once you’ve found the plugin you can view details about it such as the the point release, rating and description. Most importantly of course, you can install it by simply clicking “Install Now”.
 
 = Manual installation =
 
-The manual installation method involves downloading our iDEAL plugin and uploading it to your webserver via your favourite FTP application. The WordPress codex contains [instructions on how to do this here](http://codex.wordpress.org/Managing_Plugins#Manual_Plugin_Installation).
+The manual installation method involves downloading the plugin and uploading it to your webserver via your favourite FTP application. The WordPress codex contains [instructions on how to do this](http://codex.wordpress.org/Managing_Plugins#Manual_Plugin_Installation).
 
 = Updating =
 
