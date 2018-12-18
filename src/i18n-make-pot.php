52980bc8f3db1aeffde15927111a6095a5e454b4
Reüel van der Steege <reuel@pronamic.nl>
2018-12-18 09:57:29 +0100

    Include root PHP files in make-pot command.

--- src/i18n-make-pot.php
+++ src/i18n-make-pot.php
@@ -7,19 +7,25 @@ class MakePotCommand extends \WP_CLI\I18n\MakePotCommand {
 		parent::__construct();
 
 		// https://github.com/wp-cli/i18n-command/blob/v2.0.1/src/MakePotCommand.php#L36-L44
-		$this->exclude = array_diff( $this->exclude, array(
-			'vendor',
-		) );
+		$this->exclude = array_diff(
+			$this->exclude,
+			array(
+				'vendor',
+			)
+		);
 
-		$this->exclude = array_merge( $this->exclude, array(
-			'build',
-			'deploy',
-			'documentation',
-			'etc',
-			'repositories',
-			'wordpress',
-			'wp-content',
-		) );
+		$this->exclude = array_merge(
+			$this->exclude,
+			array(
+				'build',
+				'deploy',
+				'documentation',
+				'etc',
+				'repositories',
+				'wordpress',
+				'wp-content',
+			)
+		);
 
 		$this->include = array(
 			'admin',
@@ -27,6 +33,7 @@ class MakePotCommand extends \WP_CLI\I18n\MakePotCommand {
 			'templates',
 			'vendor',
 			'views',
+			'*.php',
 		);
 	}
 }
