634c4a27a6247b96df61a55613602ff002e58435
Reüel van der Steege <reuel@pronamic.nl>
2019-02-11 12:26:16 +0100

    Coding standards.

--- tests/wp-config.php
+++ tests/wp-config.php
@@ -1,4 +1,17 @@
 <?php
+/**
+ * Tests WordPress config.
+ *
+ * @author    Pronamic <info@pronamic.eu>
+ * @copyright 2005-2019 Pronamic
+ * @license   GPL-3.0-or-later
+ * @package   Pronamic\WordPress\Pay
+ */
+
+/*
+ * This configuration file will be used by the copy of WordPress being tested.
+ * wordpress/wp-config.php will be ignored.
+ */
 
 /* Path to the WordPress codebase you'd like to test. Add a forward slash in the end. */
 define( 'ABSPATH', dirname( dirname( __FILE__ ) ) . '/wordpress/' );
@@ -11,46 +24,49 @@ define( 'ABSPATH', dirname( dirname( __FILE__ ) ) . '/wordpress/' );
  */
 define( 'WP_DEFAULT_THEME', 'default' );
 
-// Test with multisite enabled.
-// Alternatively, use the tests/phpunit/multisite.xml configuration file.
-// define( 'WP_TESTS_MULTISITE', true );
+/*
+ * Test with multisite enabled.
+ * Alternatively, use the tests/phpunit/multisite.xml configuration file.
+ * define( 'WP_TESTS_MULTISITE', true );
+ */
 
-// Force known bugs to be run.
-// Tests with an associated Trac ticket that is still open are normally skipped.
-// define( 'WP_TESTS_FORCE_KNOWN_BUGS', true );
+/*
+ * Force known bugs to be run.
+ * Tests with an associated Trac ticket that is still open are normally skipped.
+ * define( 'WP_TESTS_FORCE_KNOWN_BUGS', true );
+ */
 
 // Test with WordPress debug mode (default).
 define( 'WP_DEBUG', true );
 
-// ** MySQL settings ** //
+/*
+ * MySQL settings
+ *
+ * WARNING WARNING WARNING!
+ * These tests will DROP ALL TABLES in the database with the prefix named below.
+ * DO NOT use a production database or one that is shared with something else.
+ */
+define( 'DB_NAME', getenv( 'WP_TESTS_DB_NAME' ) ?: 'wp_phpunit_tests' );
+define( 'DB_USER', getenv( 'WP_TESTS_DB_USER' ) ?: 'root' );
+define( 'DB_PASSWORD', getenv( 'WP_TESTS_DB_PASS' ) ?: '' );
+define( 'DB_HOST', getenv( 'WP_TESTS_DB_HOST' ) ?: 'localhost' );
+define( 'DB_CHARSET', 'utf8' );
+define( 'DB_COLLATE', '' );
 
-// This configuration file will be used by the copy of WordPress being tested.
-// wordpress/wp-config.php will be ignored.
-
-// WARNING WARNING WARNING!
-// These tests will DROP ALL TABLES in the database with the prefix named below.
-// DO NOT use a production database or one that is shared with something else.
-define( 'DB_NAME'       , getenv( 'WP_TESTS_DB_NAME' ) ?: 'wp_phpunit_tests' );
-define( 'DB_USER'       , getenv( 'WP_TESTS_DB_USER' ) ?: 'root' );
-define( 'DB_PASSWORD'   , getenv( 'WP_TESTS_DB_PASS' ) ?: '' );
-define( 'DB_HOST'       , getenv( 'WP_TESTS_DB_HOST' ) ?: 'localhost' );
-define( 'DB_CHARSET'    , 'utf8' );
-define( 'DB_COLLATE'    , '' );
-
-/**#@+
+/**
  * Authentication Unique Keys and Salts.
  *
  * Change these to different unique phrases!
  * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
  */
-define( 'AUTH_KEY',         'put your unique phrase here' );
-define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
-define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
-define( 'NONCE_KEY',        'put your unique phrase here' );
-define( 'AUTH_SALT',        'put your unique phrase here' );
+define( 'AUTH_KEY', 'put your unique phrase here' );
+define( 'SECURE_AUTH_KEY', 'put your unique phrase here' );
+define( 'LOGGED_IN_KEY', 'put your unique phrase here' );
+define( 'NONCE_KEY', 'put your unique phrase here' );
+define( 'AUTH_SALT', 'put your unique phrase here' );
 define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
-define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
-define( 'NONCE_SALT',       'put your unique phrase here' );
+define( 'LOGGED_IN_SALT', 'put your unique phrase here' );
+define( 'NONCE_SALT', 'put your unique phrase here' );
 
 define( 'WP_TESTS_DOMAIN', 'example.org' );
 define( 'WP_TESTS_EMAIL', 'admin@example.org' );
