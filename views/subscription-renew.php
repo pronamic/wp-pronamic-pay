4c1395d5f1dfe8e02c0f9391d4e2a81b948d08ae
Reüel van der Steege <reuel@pronamic.nl>
2019-02-26 16:06:02 +0100

    Register style `pronamic-pay-redirect` in plugin.

--- views/subscription-renew.php
+++ views/subscription-renew.php
@@ -8,19 +8,6 @@
  * @package   Pronamic\WordPress\Pay
  */
 
-global $pronamic_ideal;
-
-use Pronamic\WordPress\Pay\Plugin;
-
-$min = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
-
-wp_register_style(
-	'pronamic-pay-redirect',
-	plugins_url( 'css/redirect' . $min . '.css', Plugin::$file ),
-	array(),
-	$pronamic_ideal->get_version()
-);
-
 ?>
 <!DOCTYPE html>
 
