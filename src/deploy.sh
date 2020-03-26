b213e4f6ec9f2978c07fb6e340c449fa7596751b
Reüel van der Steege <reuel@pronamic.nl>
2020-03-26 17:08:11 +0100

    Update deployment script.

--- src/deploy.sh
+++ src/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Pronamic Deployer.
 if [ ! -d "deployer" ]; then
   git clone https://github.com/pronamic/deployer.git
 fi
@@ -9,5 +10,15 @@ cd deployer
 git pull
 
 composer install --no-interaction
+
+# Install AWS CLI
+# https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2-linux.html
+if ! [ -x "$(command -v aws)" ]; then
+  curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+  unzip awscliv2.zip
+  sudo ./aws/install
+
+  aws --version
+fi
 
 ./bin/pronamic-deployer deploy pronamic-ideal https://github.com/pronamic/wp-pronamic-ideal.git -vvvv --non-interactive --to-s3 --to-wp-org
