04473e9728d03ca59bb2b9e0046197412c788ede
Reüel van der Steege <reuel@pronamic.nl>
2020-03-27 16:58:55 +0100

    Make sure to autoload development files of `wp-pay/core` instead of from `vendor` directories.

--- src/autoload-before.php
+++ src/autoload-before.php
@@ -18,6 +18,12 @@ foreach ( glob( __DIR__ . '/../repositories/*/*/vendor/composer/autoload_files.p
 			continue;
 		}
 
+		// Make sure to load `wp-pay/core` files from development repository,
+		// instead of from `vendor` directory of gateways and extensions.
+		if ( false !== strpos( $filepath, '/vendor/wp-pay/core/' ) ) {
+			continue;
+		}
+
 		require $filepath;
 
 		$__composer_autoload_files[ $identifier ] = true;
