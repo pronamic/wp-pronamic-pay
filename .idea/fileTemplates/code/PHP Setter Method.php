d2189364836b63f9208793b3b7c24dd41ad51a79
Reüel van der Steege <reuel@pronamic.nl>
2021-06-18 10:29:34 +0200

    Update IDE settings.

---
+++ .idea/fileTemplates/code/PHP Setter Method.php
@@ -0,0 +1,14 @@
+#set( $method_name = $NAME.substring(1))
+/**
+ * Set ${method_name}.
+ *
+ * @param ${TYPE_HINT} $${PARAM_NAME}
+ */
+public ${STATIC} function set${NAME}(#if (${SCALAR_TYPE_HINT})${SCALAR_TYPE_HINT} #end$${PARAM_NAME})#if (${VOID_RETURN_TYPE}):void #end
+{
+#if (${STATIC} == "static")
+    self::$${FIELD_NAME} = $${PARAM_NAME};
+#else
+    $this->${FIELD_NAME} = $${PARAM_NAME};
+#end
+}
