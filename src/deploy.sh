994235f5fdb1cad5f2e8aba7646b9e12431fdea8
Reüel van der Steege <reuel@pronamic.nl>
2021-09-03 14:33:20 +0200

    Update deploy script.

--- src/deploy.sh
+++ src/deploy.sh
@@ -5,7 +5,7 @@ if [ ! -d "deployer" ]; then
   git clone https://github.com/pronamic/deployer.git
 fi
 
-cd deployer
+cd deployer || exit
 
 git pull
 
@@ -22,3 +22,8 @@ if ! [ -x "$(command -v aws)" ]; then
 fi
 
 ./bin/wp-deployer deploy pronamic-ideal https://github.com/pronamic/wp-pronamic-pay.git -vvvv --non-interactive --to-s3 --to-wp-org
+./bin/wp-deployer deploy pronamic-pay-contact-form-7 https://github.com/wp-pay-extensions/contact-form-7.git -vvvv --non-interactive --to-s3
+./bin/wp-deployer deploy pronamic-pay-adyen https://github.com/wp-pay-gateways/adyen.git -vvvv --non-interactive --to-s3
+./bin/wp-deployer deploy pronamic-pay-digiwallet https://github.com/wp-pay-gateways/digiwallet.git -vvvv --non-interactive --to-s3
+./bin/wp-deployer deploy pronamic-pay-payvision https://github.com/wp-pay-gateways/payvision.git -vvvv --non-interactive --to-s3
+./bin/wp-deployer deploy pronamic-pay-fundraising https://github.com/wp-pay/fundraising.git -vvvv --non-interactive --to-s3
