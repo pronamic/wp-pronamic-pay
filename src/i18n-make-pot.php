d860233e5064f93c812ac3f2e27c63bd8e237e93
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2022-12-13 12:32:16 +0100

    Move custom command class to it's own file.

--- src/i18n-make-pot.php
+++ src/i18n-make-pot.php
@@ -14,7 +14,7 @@ if ( ! \class_exists( 'WP_CLI' ) ) {
 	return;
 }
 
-/*
+/**
  * The `pronamic i18n make-pot` command requires the `i18n make-pot` command.
  *
  * @link https://make.wordpress.org/cli/2017/05/03/managing-command-dependencies/
@@ -22,65 +22,17 @@ if ( ! \class_exists( 'WP_CLI' ) ) {
 \WP_CLI::add_hook(
 	'after_add_command:i18n make-pot',
 	function () {
+		require_once __DIR__ . '/class-make-pot-command.php';
 
 		/**
-		 * Title: Make pot command.
-		 * Description:
-		 * Copyright: 2005-2022 Pronamic
-		 * Company: Pronamic
+		 * Add command `pronamic i18n make-pot`.
 		 *
-		 * @author  Remco Tolsma
-		 * @version 5.5.5
-		 * @since   5.5.0
-		 */
-		class MakePotCommand extends \WP_CLI\I18n\MakePotCommand {
-			/**
-			 * Command constructor.
-			 */
-			public function __construct() {
-				parent::__construct();
-
-				// @link https://github.com/wp-cli/i18n-command/blob/v2.0.1/src/MakePotCommand.php#L36-L44
-				$this->exclude = array_diff(
-					$this->exclude,
-					[
-						'vendor',
-					]
-				);
-
-				$this->exclude = array_merge(
-					$this->exclude,
-					[
-						'build',
-						'deploy',
-						'documentation',
-						'etc',
-						'repositories',
-						'vendor/wp-phpunit',
-						'vendor-bin',
-						'wordpress',
-						'wp-content',
-					]
-				);
-
-				$this->include = [
-					'admin',
-					'includes',
-					'templates',
-					'vendor',
-					'views',
-					'*.php',
-				];
-			}
-		}
-
-		// @link https://github.com/wp-cli/i18n-command/blob/v2.0.1/i18n-command.php
-		\WP_CLI::add_command( 'pronamic i18n make-pot', '\Pronamic\WordPress\Pay\MakePotCommand' );
-
-		/*
 		 * Usage example:
 		 *
 		 * wp pronamic i18n make-pot . languages/pronamic_ideal.pot --slug="pronamic-ideal"
+		 * 
+		 * @link https://github.com/wp-cli/i18n-command/blob/v2.0.1/i18n-command.php
 		 */
+		\WP_CLI::add_command( 'pronamic i18n make-pot', '\Pronamic\WordPress\Pay\MakePotCommand' );
 	}
 );
