828a537a9da80870cd3f0c59254ffd623164b8f6
Reüel van der Steege <reuel@pronamic.nl>
2023-01-02 15:43:47 +0100

    Update deploy script to use `pronamic/wp-deployer` dev dependency.

--- src/deploy.sh
+++ src/deploy.sh
@@ -3,15 +3,6 @@
 cd `dirname $0`
 cd ..
 
-# Pronamic Deployer.
-if [ ! -d "deployer" ]; then
-  git clone https://github.com/pronamic/deployer.git
-fi
-
-cd deployer || exit
-
-git pull
-
 composer install --no-interaction
 
 # Install AWS CLI
@@ -24,9 +15,21 @@ if ! [ -x "$(command -v aws)" ]; then
   aws --version
 fi
 
-./bin/wp-deployer deploy pronamic-ideal https://github.com/pronamic/wp-pronamic-pay.git -vvvv --non-interactive --to-s3 --to-wp-org
-./bin/wp-deployer deploy pronamic-pay-contact-form-7 https://github.com/wp-pay-extensions/contact-form-7.git -vvvv --non-interactive --to-s3
-./bin/wp-deployer deploy pronamic-pay-adyen https://github.com/wp-pay-gateways/adyen.git -vvvv --non-interactive --to-s3
-./bin/wp-deployer deploy pronamic-pay-digiwallet https://github.com/wp-pay-gateways/digiwallet.git --branch main -vvvv --non-interactive --to-s3
-./bin/wp-deployer deploy pronamic-pay-paypal https://github.com/wp-pay-gateways/paypal.git -vvvv --non-interactive --to-s3
-./bin/wp-deployer deploy pronamic-pay-fundraising https://github.com/wp-pay/fundraising.git -vvvv --non-interactive --to-s3
+./vendor/bin/wp-deployer deploy pronamic-ideal https://github.com/pronamic/wp-pronamic-pay.git -vvvv --branch main --non-interactive --to-s3 --to-wp-org
+./vendor/bin/wp-deployer deploy pronamic-pay-adyen https://github.com/pronamic/wp-pronamic-pay-adyen.git -vvvv --branch main --non-interactive --to-s3
+./vendor/bin/wp-deployer deploy pronamic-pay-buckaroo https://github.com/pronamic/wp-pronamic-pay-buckaroo.git -vvvv --branch master --non-interactive --to-s3
+./vendor/bin/wp-deployer deploy pronamic-pay-charitable https://github.com/pronamic/wp-pronamic-pay-charitable.git -vvvv --branch main --non-interactive --to-s3
+./vendor/bin/wp-deployer deploy pronamic-pay-contact-form-7 https://github.com/pronamic/wp-pronamic-pay-contact-form-7.git -vvvv --branch main --non-interactive --to-s3
+./vendor/bin/wp-deployer deploy pronamic-pay-digiwallet https://github.com/pronamic/wp-pronamic-pay-digiwallet.git -vvvv --branch main --non-interactive --to-s3
+./vendor/bin/wp-deployer deploy pronamic-pay-easy-digital-downloads https://github.com/pronamic/wp-pronamic-pay-easy-digital-downloads.git -vvvv --branch main --non-interactive --to-s3
+./vendor/bin/wp-deployer deploy pronamic-pay-event-espresso https://github.com/pronamic/wp-pronamic-pay-event-espresso.git -vvvv --branch main --non-interactive --to-s3
+./vendor/bin/wp-deployer deploy pronamic-pay-formidable-forms https://github.com/pronamic/wp-pronamic-pay-formidable-forms.git -vvvv --branch main --non-interactive --to-s3
+./vendor/bin/wp-deployer deploy pronamic-pay-fundraising https://github.com/pronamic/wp-pronamic-pay-fundraising.git -vvvv --branch main --non-interactive --to-s3
+./vendor/bin/wp-deployer deploy pronamic-pay-give https://github.com/pronamic/wp-pronamic-pay-give.git -vvvv --branch main --non-interactive --to-s3
+./vendor/bin/wp-deployer deploy pronamic-pay-gravitforms https://github.com/pronamic/wp-pronamic-pay-gravitforms.git -vvvv --branch main --non-interactive --to-s3
+./vendor/bin/wp-deployer deploy pronamic-pay-memberpress https://github.com/pronamic/wp-pronamic-pay-memberpress.git -vvvv --branch main --non-interactive --to-s3
+./vendor/bin/wp-deployer deploy pronamic-pay-mollie https://github.com/pronamic/wp-pronamic-pay-mollie.git -vvvv --branch main --non-interactive --to-s3
+./vendor/bin/wp-deployer deploy pronamic-pay-ninjaforms https://github.com/pronamic/wp-pronamic-pay-ninjaforms.git -vvvv --branch main --non-interactive --to-s3
+./vendor/bin/wp-deployer deploy pronamic-pay-paypal https://github.com/pronamic/wp-pronamic-pay-paypal.git -vvvv --branch main --non-interactive --to-s3
+./vendor/bin/wp-deployer deploy pronamic-pay-restrict-content-pro https://github.com/pronamic/wp-pronamic-pay-restrict-content-pro.git -vvvv --branch main --non-interactive --to-s3
+./vendor/bin/wp-deployer deploy pronamic-pay-woocommerce https://github.com/pronamic/wp-pronamic-pay-woocommerce.git -vvvv --branch main --non-interactive --to-s3
